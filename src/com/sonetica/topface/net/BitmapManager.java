2f1314cde411a94812cfe8308f088011359460f9
onepavel <pal2mal@gmail.com>
2011-11-14 18:33:50 +0400

    + менеджер изображений

---
+++ src/com/sonetica/topface/net/BitmapManager.java
@@ -0,0 +1,15 @@
+package com.sonetica.topface.net;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class BitmapManager {
+  // Data
+  private ArrayList  mUrls; //список ссылок на изображения полученные из JSON ответа topfase сервера
+  private LinkedList mQueue; //очередь ожидающих вьюшек на подгрузку битмапа
+  //---------------------------------------------------------------------------
+  public BitmapManager() {
+    
+  }
+  //---------------------------------------------------------------------------
+}
