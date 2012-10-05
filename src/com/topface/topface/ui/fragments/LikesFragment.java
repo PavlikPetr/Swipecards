7e7221f8736e4f04d5143be6fb27106cd8022f19
Andrey Mischenko <git@gildor.ru>
2012-10-05 13:59:27 +0400

    очепятка

--- src/com/topface/topface/ui/fragments/LikesFragment.java
+++ src/com/topface/topface/ui/fragments/LikesFragment.java
@@ -48,7 +48,7 @@ public class LikesFragment extends FeedFragment<LikesListAdapter> {
         registerRequest(likesRequest);
         likesRequest.limit = FeedAdapter.LIMIT;
         likesRequest.unread = mDoubleButton.isRightButtonChecked();
-        FeedLike lastLike = mListAdapter.getLastFeedItxem();
+        FeedLike lastLike = mListAdapter.getLastFeedItem();
         if (isHistoryLoad && lastLike != null) {
             likesRequest.from = lastLike.id;
         }
