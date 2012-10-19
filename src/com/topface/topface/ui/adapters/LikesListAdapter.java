23ae78f6ac33c8a0de2cd5174d52f204bf42c5a0
Andrey Mischenko <git@gildor.ru>
2012-10-19 15:28:57 +0400

    fix uid

--- src/com/topface/topface/ui/adapters/LikesListAdapter.java
+++ src/com/topface/topface/ui/adapters/LikesListAdapter.java
@@ -6,41 +6,39 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
-
 import com.topface.topface.R;
 import com.topface.topface.data.FeedLike;
 
 public class LikesListAdapter extends FeedAdapter<FeedLike> {
-	private int mSelectedForMutual = -1;
-	private int mPrevSelectedForMutual = -1;
-	
-	private int T_SELETED_FOR_MUTUAL = 3;
+    private int mSelectedForMutual = -1;
+    private int mPrevSelectedForMutual = -1;
+
+    private int T_SELETED_FOR_MUTUAL = 3;
     private int T_COUNT = 1;
-	
-    private OnMutualListener mMutualListener;    
-    
+
+    private OnMutualListener mMutualListener;
+
     public interface OnMutualListener {
-    	void onMutual(int userId,int rate,int mutualId);
+        void onMutual(int userId, int rate, int mutualId);
     }
-    
+
     public LikesListAdapter(Context context, Updater updateCallback) {
-        super(context, updateCallback);        
-    }    
-    
-    @Override
-    public int getItemViewType(int position) {  
-    	if (mSelectedForMutual == position && !getItem(position).mutualed) {    		
-    		return T_SELETED_FOR_MUTUAL;
-    	}
-    	else return super.getItemViewType(position);
+        super(context, updateCallback);
     }
-    
+
+    @Override
+    public int getItemViewType(int position) {
+        if (mSelectedForMutual == position && !getItem(position).mutualed) {
+            return T_SELETED_FOR_MUTUAL;
+        } else return super.getItemViewType(position);
+    }
+
     @Override
     public int getViewTypeCount() {
-    	
-    	return (super.getViewTypeCount()+T_COUNT);
+
+        return (super.getViewTypeCount() + T_COUNT);
     }
-    
+
     @Override
     protected View getContentView(final int position, View convertView, ViewGroup viewGroup) {
         convertView = super.getContentView(position, convertView, viewGroup);
@@ -48,63 +46,63 @@ public class LikesListAdapter extends FeedAdapter<FeedLike> {
         final FeedLike like = getItem(position);
 
         holder.heart.setImageResource(like.highrate ?
-                R.drawable.im_item_mutual_heart_top : 
+                R.drawable.im_item_mutual_heart_top :
                 (like.mutualed ? R.drawable.im_item_dbl_mutual_heart : R.drawable.im_item_mutual_heart));
-                
-        final ViewFlipper vf = holder.flipper;
-        
-        holder.heart.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				setSelectedForMutual(position);	
-			}
-		});
-        
-        if(position == mSelectedForMutual) {
-        	vf.setInAnimation(getContext(),R.anim.slide_in_from_right);
-	        vf.setOutAnimation(getContext(),android.R.anim.fade_out);
-	        vf.setDisplayedChild(1);
-			convertView.setActivated(true);
-			holder.flippedBtn.setOnClickListener(new OnClickListener() {
 
-				@Override
-				public void onClick(View v) {
-					if (mMutualListener != null) {
-						mMutualListener.onMutual(getItem(position).uid, 9,like.id);
-						setSelectedForMutual(-1);
-						like.mutualed = true;
-					}
-				}
-			});
+        final ViewFlipper vf = holder.flipper;
+
+        holder.heart.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setSelectedForMutual(position);
+            }
+        });
+
+        if (position == mSelectedForMutual) {
+            vf.setInAnimation(getContext(), R.anim.slide_in_from_right);
+            vf.setOutAnimation(getContext(), android.R.anim.fade_out);
+            vf.setDisplayedChild(1);
+            convertView.setActivated(true);
+            holder.flippedBtn.setOnClickListener(new OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    if (mMutualListener != null) {
+                        mMutualListener.onMutual(getItem(position).user.id, 9, like.id);
+                        setSelectedForMutual(-1);
+                        like.mutualed = true;
+                    }
+                }
+            });
 
         } else {
-        	if (mPrevSelectedForMutual == position) {
-        		vf.setInAnimation(getContext(),android.R.anim.fade_in);
-            	vf.setOutAnimation(getContext(),R.anim.slide_out_right);
-        		vf.setDisplayedChild(0);
-        		convertView.setActivated(false);
-        		mPrevSelectedForMutual = -1;
-        	}
-        }        
-                
+            if (mPrevSelectedForMutual == position) {
+                vf.setInAnimation(getContext(), android.R.anim.fade_in);
+                vf.setOutAnimation(getContext(), R.anim.slide_out_right);
+                vf.setDisplayedChild(0);
+                convertView.setActivated(false);
+                mPrevSelectedForMutual = -1;
+            }
+        }
+
         return convertView;
     }
 
     public void setSelectedForMutual(int position) {
-    	if(position != -1) {
-	    	if(!getItem(position).mutualed) {
-		    	mPrevSelectedForMutual = mSelectedForMutual;
-				mSelectedForMutual = position;
-				notifyDataSetChanged();
-	    	}
-    	} else {
-    		mPrevSelectedForMutual = mSelectedForMutual;
-			mSelectedForMutual = position;
-			notifyDataSetChanged();
-    	}
+        if (position != -1) {
+            if (!getItem(position).mutualed) {
+                mPrevSelectedForMutual = mSelectedForMutual;
+                mSelectedForMutual = position;
+                notifyDataSetChanged();
+            }
+        } else {
+            mPrevSelectedForMutual = mSelectedForMutual;
+            mSelectedForMutual = position;
+            notifyDataSetChanged();
+        }
     }
-        
+
     @Override
     protected FeedViewHolder getEmptyHolder(View convertView, FeedLike item) {
         FeedViewHolder holder = super.getEmptyHolder(convertView, item);
@@ -116,9 +114,9 @@ public class LikesListAdapter extends FeedAdapter<FeedLike> {
     protected int getItemLayout() {
         return R.layout.item_feed_like;
     }
-    
+
     public void setOnMutualListener(OnMutualListener listener) {
-    	mMutualListener = listener;
-    }        
+        mMutualListener = listener;
+    }
 
 }
