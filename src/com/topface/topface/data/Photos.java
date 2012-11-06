8fdfb56ca145e902b1d07f7afb9e724133276601
Andrey Mischenko <git@gildor.ru>
2012-11-06 10:43:30 +0400

    Фиксы верстки

--- src/com/topface/topface/data/Photos.java
+++ src/com/topface/topface/data/Photos.java
@@ -7,7 +7,7 @@ import org.json.JSONException;
 
 import java.util.ArrayList;
 
-public class Photos extends ArrayList<Photo>{
+public class Photos extends ArrayList<Photo> {
 
     public Photos(JSONArray photos) {
         this();
@@ -67,46 +67,14 @@ public class Photos extends ArrayList<Photo>{
     }
 
     public Photo getFirst() {
-    	if (!this.isEmpty())
-    		return this.get(0);
-    	else return null;
+        if (!this.isEmpty()) {
+            return this.get(0);
+        }
+
+        return null;
     }
-    
+
     public void addFirst(Photo value) {
-    	if (this != null)
-    		this.add(0,value);
+        this.add(0, value);
     }
-    
-//	@Override
-//	public int describeContents() {
-//		// TODO Auto-generated method stub
-//		return 0;
-//	}
-//
-//	@Override
-//	public void writeToParcel(Parcel dest, int flags) {		
-//		dest.writeInt(this.size());
-//		for (int i = 0; i < this.size(); i++) {
-//			dest.writeParcelable(this.get(i), 0);
-//		}
-//	}
-//	
-//	@SuppressWarnings("rawtypes")
-//	public static final Parcelable.Creator CREATOR =
-//	    	new Parcelable.Creator() {
-//	            public Photos createFromParcel(Parcel in) {
-//	            	Photos photos = new Photos();
-//	            	int size = in.readInt();
-//	            	for (int i = 0; i < size; i++) {
-//	            		Photo photo = in.readParcelable(Photo.class.getClassLoader()); 
-//						photos.add(photo);
-//					}	            		            	
-//	            	
-//	                return photos;
-//	            }
-//	 
-//	            public Photos[] newArray(int size) {
-//	                return new Photos[size];
-//	            }
-//	        };
 }
