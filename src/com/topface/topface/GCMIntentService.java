1474c0f7e033f86193eb607e05137c64898a899a
iovorobiev <iovorobiev@gmail.com>
2012-11-06 11:57:07 +0400

    added notifications

---
+++ src/com/topface/topface/GCMIntentService.java
@@ -0,0 +1,11 @@
+package com.topface.topface;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ilya Vorobiev
+ * Date: 31.10.12
+ * Time: 15:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class GCMIntentService {
+}
---
+++ src/com/topface/topface/GCMUtils.java
@@ -0,0 +1,11 @@
+package com.topface.topface;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ilya Vorobiev
+ * Date: 31.10.12
+ * Time: 16:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class GCMUtils {
+}
