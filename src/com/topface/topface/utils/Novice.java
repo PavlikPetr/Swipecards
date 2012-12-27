89e210ce21009e49d9354ba39f7c7a2225687437
kirussell <kirussellkirussell@gmail.com>
2012-12-27 13:11:27 +0400

    подсказки для лайков

--- src/com/topface/topface/utils/Novice.java
+++ src/com/topface/topface/utils/Novice.java
@@ -5,44 +5,96 @@ import android.content.SharedPreferences;
 import com.topface.topface.Static;
 
 public class Novice {
-    public static boolean giveNoviceLikes = false;
+    public static final int MIN_LIKES_QUANTITY = 3;
 
-    public boolean showSympathy;
-    public boolean showBatteryBonus;
-    public boolean showEnergy;
-    public boolean showFillProfile;
+    public static boolean giveNoviceLikes = false;
+    public static int giveNoviceLikesQuantity = 0;
+
+    private Boolean showEnergyToSympathies = null;
+    private Boolean showSympathy = null;
+    private Boolean showSympathiesBonus = null;
+    private Boolean showBuySympathies = null;
+    private Boolean showFillProfile = null;
 
     private SharedPreferences mPreferences;
 
-    public Novice(SharedPreferences preferences) {
-        mPreferences = preferences;
+    private static Novice mInstance;
 
-        showSympathy = mPreferences.getBoolean(Static.PREFERENCES_NOVICE_DATING_SYMPATHY, true);
-        showEnergy = getShowEnergy(); //preferences.getBoolean(Static.PREFERENCES_NOVICE_DATING_ENERGY, true);
-        showBatteryBonus = Novice.giveNoviceLikes;
-        showFillProfile = getShowProfile();//preferences.getBoolean(Static.PREFERENCES_NOVICE_MENU_FILL_PROFILE, true);        
+    public static Novice getInstance(SharedPreferences preferences) {
+        if (mInstance == null) {
+            mInstance = new Novice(preferences);
+        }
+        return mInstance;
+    }
+
+    private Novice(SharedPreferences preferences) {
+        mPreferences = preferences;
+    }
+
+    public boolean isShowEnergyToSympathies() {
+        if (showEnergyToSympathies == null) {
+            showEnergyToSympathies = getShowEnergyToSympathies();
+        }
+        return showEnergyToSympathies;
+    }
+
+    public boolean isShowSympathy() {
+        if (showSympathy == null) {
+            showSympathy = mPreferences.getBoolean(Static.PREFERENCES_NOVICE_DATING_SYMPATHY, true);
+        }
+        return showSympathy;
+    }
+
+    public boolean isShowSympathiesBonus() {
+        return Novice.giveNoviceLikes;
+    }
+
+    public boolean isShowBuySympathies() {
+        if (showBuySympathies == null) {
+            showBuySympathies = getShowBuySympathies();
+        }
+        return showBuySympathies;
+    }
+
+    public boolean isShowFillProfile() {
+        if (showFillProfile == null) {
+            showFillProfile = getShowProfile();
+        }
+        return showFillProfile;
     }
 
     public boolean isMenuCompleted() {
-        return !showFillProfile;
+        return !isShowFillProfile();
     }
 
     public boolean isDatingCompleted() {
-        return !showBatteryBonus & !showEnergy & !showSympathy;
+        return !isShowSympathiesBonus() & !isShowBuySympathies() & !isShowSympathy() & !isShowEnergyToSympathies();
     }
 
-    private boolean getShowEnergy() {
-        boolean result = mPreferences.getBoolean(Static.PREFERENCES_NOVICE_DATING_ENERGY, true);
+    private boolean getShowEnergyToSympathies() {
+        if (mPreferences.contains(Static.PREFERENCES_NOVICE_DATING_ENERGY_TO_SYMPATHIES)) {
+            return mPreferences.getBoolean(Static.PREFERENCES_NOVICE_DATING_ENERGY_TO_SYMPATHIES, false);
+        } else {
+            boolean result = mPreferences.contains(Static.PREFERENCES_NOVICE_DATING_ENERGY);
+            SharedPreferences.Editor editor = mPreferences.edit();
+            editor.putBoolean(Static.PREFERENCES_NOVICE_DATING_ENERGY_TO_SYMPATHIES, false);
+            editor.commit();
+            return result;
+        }
+    }
+
+    private boolean getShowBuySympathies() {
+        boolean result = mPreferences.getBoolean(Static.PREFERENCES_NOVICE_DATING_BUY_SYMPATHY, true);
         if (result) return true;
 
         long todayTime = Utils.unixtime();
-        long lastTime = mPreferences.getLong(Static.PREFERENCES_NOVICE_DATING_ENERGY_DATE, 0);
+        long lastTime = mPreferences.getLong(Static.PREFERENCES_NOVICE_DATING_BUY_SYMPATHY_DATE, 0);
 
         if (lastTime > 0) {
             return (todayTime - lastTime) >= Utils.WEEK;
         } else {
             SharedPreferences.Editor editor = mPreferences.edit();
-            editor.putLong(Static.PREFERENCES_NOVICE_DATING_ENERGY_DATE, todayTime);
+            editor.putLong(Static.PREFERENCES_NOVICE_DATING_BUY_SYMPATHY_DATE, todayTime);
             editor.commit();
             return false;
         }
@@ -71,11 +123,11 @@ public class Novice {
         editor.commit();
     }
 
-    public void completeShowEnergy() {
-        showEnergy = false;
+    public void completeShowBuySympathies() {
+        showBuySympathies = false;
         SharedPreferences.Editor editor = mPreferences.edit();
-        editor.putLong(Static.PREFERENCES_NOVICE_DATING_ENERGY_DATE, Utils.unixtime());
-        editor.putBoolean(Static.PREFERENCES_NOVICE_DATING_ENERGY, false);
+        editor.putLong(Static.PREFERENCES_NOVICE_DATING_BUY_SYMPATHY_DATE, Utils.unixtime());
+        editor.putBoolean(Static.PREFERENCES_NOVICE_DATING_BUY_SYMPATHY, false);
         editor.commit();
     }
 
@@ -87,7 +139,7 @@ public class Novice {
     }
 
     public void completeShowBatteryBonus() {
-        showBatteryBonus = false;
+        showSympathiesBonus = false;
         Novice.giveNoviceLikes = false;
     }
 }
