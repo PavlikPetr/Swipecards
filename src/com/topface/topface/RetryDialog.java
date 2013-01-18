c3590ed539217a7d8f4371401925da9b7614dbaf
iovorobiev <iovorobiev@gmail.com>
2013-01-18 17:50:01 +0400

    Поправил анимацию в диалоге дисконнекта, подредактировал pngшки

--- src/com/topface/topface/RetryDialog.java
+++ src/com/topface/topface/RetryDialog.java
@@ -8,11 +8,16 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import com.topface.topface.receivers.ConnectionChangeReceiver;
 import com.topface.topface.requests.ApiRequest;
 import com.topface.topface.ui.views.ImageViewRemote;
 import com.topface.topface.ui.views.SpriteTile;
+import com.topface.topface.utils.Debug;
 
 /**
  * Диалог, показываемый при ошибке отправки запроса и предлагающий его повторить
@@ -32,6 +37,32 @@ public class RetryDialog extends AlertDialog {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.retry_dialog_layout);
+
+        ImageView satelite = (ImageView) findViewById(R.id.sat);
+        Animation anim = new RotateAnimation(0f, 358f,Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+
+        anim.setRepeatCount(Animation.INFINITE);
+        anim.setDuration(3000);
+        anim.setInterpolator(new LinearInterpolator());
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                Debug.log("ANIMATION:: start");
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Debug.log("ANIMATION:: end");
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                Debug.log("ANIMATION:: repeat");
+            }
+        });
+//        satelite.setAnimation(anim);
+        satelite.startAnimation(anim);
+
         mReciever = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
