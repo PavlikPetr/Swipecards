be7a77ced8898b43bd3ef757ad4570d83ccca093
iovorobiev <iovorobiev@gmail.com>
2013-02-04 19:11:41 +0400

    Добавил запрос ChangePassword и исправил ChangeLogin

---
+++ src/com/topface/topface/requests/ChangePasswordRequest.java
@@ -0,0 +1,11 @@
+package com.topface.topface.requests;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ilya
+ * Date: 2/4/13
+ * Time: 7:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ChangePasswordRequest {
+}
