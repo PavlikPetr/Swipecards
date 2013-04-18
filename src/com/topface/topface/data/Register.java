36087fb2026c5a0435e1b999cfef41bd6a330c31
kirussell <kirussellkirussell@gmail.com>
2013-04-18 18:57:25 +0400

    - Пустые данные в фидбеке со стенделона  #15459

--- src/com/topface/topface/data/Register.java
+++ src/com/topface/topface/data/Register.java
@@ -5,15 +5,15 @@ import com.topface.topface.requests.ApiResponse;
 public class Register extends AbstractData{
 
     public static final String FIELD_USER_ID = "user_id";
-    private int mUserId;
+    private String mUserId;
 
     public Register(ApiResponse response) {
         if(response != null && response.jsonResult != null) {
-            mUserId = response.jsonResult.optInt(FIELD_USER_ID);
+            mUserId = response.jsonResult.optString(FIELD_USER_ID);
         }
     }
 
-   public int getUserId() {
+   public String getUserId() {
        return mUserId;
    }
 }
