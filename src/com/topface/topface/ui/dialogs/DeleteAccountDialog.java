470fc27e4de402fd9d7fe1ef1c678c8d76b396b1
kirussell <kirussellkirussell@gmail.com>
2013-06-20 13:11:39 +0400

    - Удаление профиля пользователя #16836 (внесены изменения в диалог на основе замечаний от Юры)

--- src/com/topface/topface/ui/dialogs/DeleteAccountDialog.java
+++ src/com/topface/topface/ui/dialogs/DeleteAccountDialog.java
@@ -34,8 +34,6 @@ import java.util.List;
 public class DeleteAccountDialog extends DialogFragment implements View.OnClickListener {
 
     public static final String TAG = "com.topface.topface.ui.dialogs.DeleteAccountDialog_TAG";
-    private EditText mExtraMessage;
-    private Spinner mSpinner;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -51,47 +49,9 @@ public class DeleteAccountDialog extends DialogFragment implements View.OnClickL
         ((ImageViewRemote) root.findViewById(R.id.ivAvatar)).setPhoto(CacheProfile.photo);
         ((TextView) root.findViewById(R.id.tvProfile)).setText(CacheProfile.getUserNameAgeString());
 
-        TextView socialAccountText = ((TextView) root.findViewById(R.id.tvAccount));
-        setSocialAccountText(socialAccountText);
-
         ((TextView) root.findViewById(R.id.tvWarningText)).setText(R.string.delete_account_warning);
 
-        mExtraMessage = (EditText) root.findViewById(R.id.edReasonMessage);
-        mExtraMessage.setVisibility(View.VISIBLE);
-
-        mSpinner = (Spinner) root.findViewById(R.id.spReason);
-        final String[] reasons = getResources().getStringArray(R.array.delete_account_reasons);
-        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(
-                getActivity(),
-                android.R.layout.simple_spinner_item,
-                reasons
-        );
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mSpinner.setAdapter(adapter);
-        // last reason points to extra message
-        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if (position < parent.getCount() - 1) {
-                    mExtraMessage.setVisibility(View.GONE);
-                } else {
-                    mExtraMessage.setVisibility(View.VISIBLE);
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                mSpinner.setSelection(reasons.length - 1);
-            }
-        });
-        mSpinner.setSelection(reasons.length - 1);
-
         return root;
-    }
-
-    private void setSocialAccountText(final TextView textView) {
-        Settings.getInstance().getSocialAccountName(textView);
-        Settings.getInstance().getSocialAccountIcon(textView);
     }
 
     private void setTransparentBackground() {
@@ -108,13 +68,6 @@ public class DeleteAccountDialog extends DialogFragment implements View.OnClickL
                 closeDialog();
                 break;
             case R.id.btnOk:
-                boolean checked = false;
-                if (mExtraMessage.getVisibility() == View.VISIBLE) {
-                    if (mExtraMessage.getText().toString().trim().isEmpty()) {
-                        Toast.makeText(getActivity(), R.string.empty_fields, Toast.LENGTH_LONG).show();
-                        break;
-                    }
-                }
 
                 new AlertDialog.Builder(getActivity())
                         .setTitle(R.string.settings_delete_account)
@@ -127,8 +80,8 @@ public class DeleteAccountDialog extends DialogFragment implements View.OnClickL
                                 progress.setMessage(getActivity().getResources().getString(R.string.settings_delete_account));
                                 progress.show();
 
-                                ProfileDelete request = new ProfileDelete(mSpinner.getSelectedItemPosition() + 1,
-                                        mExtraMessage.getText().toString().trim(), getActivity());
+                                ProfileDelete request = new ProfileDelete(5,
+                                        "From Android Device", getActivity());
                                 request.callback(new ApiHandler() {
                                     @Override
                                     public void success(ApiResponse response) {
