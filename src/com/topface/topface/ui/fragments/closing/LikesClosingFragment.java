4283dd12239f467960eb3694784430527c661e78
Andrey Mischenko <git@gildor.ru>
2013-08-17 20:49:49 +0400

    Поправлено падение при отсутсвии текущего юзера - https://rink.hockeyapp.net/manage/apps/26531/app_versions/38/crash_reasons/6025646

--- src/com/topface/topface/ui/fragments/closing/LikesClosingFragment.java
+++ src/com/topface/topface/ui/fragments/closing/LikesClosingFragment.java
@@ -91,9 +91,9 @@ public class LikesClosingFragment extends ClosingFragment implements View.OnClic
         switch (v.getId()) {
             case R.id.btnMutual:
                 EasyTracker.getTracker().trackEvent(getTrackName(), "Mutual", "", 1L);
-                RateController rateController = getRateController();
-                if (rateController != null) {
-                    rateController.onRate(getCurrentUser().id, 9, RateRequest.DEFAULT_MUTUAL, new RateController.OnRateListener() {
+                FeedUser currentUser = getCurrentUser();
+                if (currentUser != null) {
+                    getRateController().onRate(currentUser.id, 9, RateRequest.DEFAULT_MUTUAL, new RateController.OnRateListener() {
                         @Override
                         public void onRateCompleted() {
                             if (isAdded()) refreshActionBarTitles(getView());
@@ -103,8 +103,8 @@ public class LikesClosingFragment extends ClosingFragment implements View.OnClic
                         public void onRateFailed() {
                         }
                     });
-                    showNextUser();
                 }
+                showNextUser();
                 break;
             default:
                 super.onClick(v);
