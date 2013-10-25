a967849f333d6bb225022b79ac3195e3f40dee93
kirussell <kirussellkirussell@gmail.com>
2013-10-25 14:21:21 +0400

    - Восстановление аккаунта #20310 - убрал из ApiResponse.isWrongAuthError() ошибку USER_DELETED, обрабатываем отдельно, чтобы не стирать AuthToken при проверке checkAuthError(...)

--- src/main/java/com/topface/topface/requests/ApiResponse.java
+++ src/main/java/com/topface/topface/requests/ApiResponse.java
@@ -145,8 +145,7 @@ public class ApiResponse implements IApiResponse, SerializableToJson {
                 ErrorCodes.UNKNOWN_SOCIAL_USER,
                 ErrorCodes.UNVERIFIED_TOKEN,
                 ErrorCodes.INCORRECT_LOGIN,
-                ErrorCodes.INCORRECT_PASSWORD,
-                ErrorCodes.USER_DELETED
+                ErrorCodes.INCORRECT_PASSWORD
         );
     }
 
