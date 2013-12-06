107e61f2ce58f41c7e7819f791173da089afedd5
Andrey Mischenko <git@gildor.ru>
2013-12-06 14:52:00 +0400

    Откатил изменения с поледнего мерджа, похоже смержены были не правильно

--- src/main/java/com/topface/topface/ui/fragments/feed/LikesFragment.java
+++ src/main/java/com/topface/topface/ui/fragments/feed/LikesFragment.java
@@ -28,7 +28,6 @@ import com.topface.topface.requests.SendLikeRequest;
 import com.topface.topface.requests.handlers.ErrorCodes;
 import com.topface.topface.requests.handlers.SimpleApiHandler;
 import com.topface.topface.ui.ContainerActivity;
-import com.topface.topface.ui.NavigationActivity;
 import com.topface.topface.ui.adapters.LikesListAdapter;
 import com.topface.topface.ui.adapters.LikesListAdapter.OnMutualListener;
 import com.topface.topface.ui.fragments.MenuFragment;
@@ -112,53 +111,179 @@ public class LikesFragment extends FeedFragment<FeedLike> {
     @Override
     protected void initEmptyFeedView(final View inflated, int errorCode) {
         if (mEmptyFeedView == null) mEmptyFeedView = inflated;
+        ViewFlipper viewFlipper = (ViewFlipper) inflated.findViewById(R.id.vfEmptyViews);
         if (CacheProfile.premium) {
-            ((ViewFlipper) inflated.findViewById(R.id.vfEmptyViews)).setDisplayedChild(0);
-            inflated.findViewById(R.id.btnStartRate).setOnClickListener(new View.OnClickListener() {
+            final Options.BlockSympathy blockSympathyOptions = CacheProfile.getOptions().blockSympathy;
+            if (blockSympathyOptions.enabled && errorCode == ErrorCodes.BLOCKED_SYMPATHIES) {
+                initEmptyScreenOnBlockedLikes(inflated, viewFlipper, blockSympathyOptions);
+            } else {
+                initEmptyScreenWithoutLikes(viewFlipper);
+            }
+        } else {
+            if (CacheProfile.unread_likes > 0) {
+                initEmptyScreenOnLikesNeedVip(viewFlipper);
+            } else {
+                initEmptyScreenWithoutLikes(viewFlipper);
+            }
+        }
+    }
+
+    private void initEmptyScreenOnLikesNeedVip(ViewFlipper viewFlipper) {
+        viewFlipper.setDisplayedChild(1);
+        View currentView = viewFlipper.getChildAt(1);
+        if (currentView != null) {
+            String title = Utils.getQuantityString(R.plurals.you_was_liked, CacheProfile.unread_likes, CacheProfile.unread_likes);
+            ((TextView) currentView.findViewById(R.id.tvTitle)).setText(title);
+            currentView.findViewById(R.id.btnBuyVip).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = ContainerActivity.getVipBuyIntent(null, "Likes");
+                    startActivityForResult(intent, ContainerActivity.INTENT_BUY_VIP_FRAGMENT);
+                }
+            });
+            currentView.findViewById(R.id.btnRate).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    MenuFragment.selectFragment(FragmentId.F_DATING);
+                }
+            });
+            initAvatarImagesToEmptyView(currentView);
+        }
+    }
+
+    private void initEmptyScreenWithoutLikes(ViewFlipper viewFlipper) {
+        viewFlipper.setDisplayedChild(0);
+        View currentView = viewFlipper.getChildAt(0);
+        if (currentView != null) {
+            currentView.findViewById(R.id.btnStartRate).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     startActivity(ContainerActivity.getBuyingIntent("EmptyLikes"));
                 }
             });
-        } else {
-            if (CacheProfile.unread_likes > 0) {
-                ((ViewFlipper) inflated.findViewById(R.id.vfEmptyViews)).setDisplayedChild(1);
-                String title = Utils.getQuantityString(R.plurals.you_was_liked, CacheProfile.unread_likes, CacheProfile.unread_likes);
-                ((TextView) inflated.findViewById(R.id.tvTitle)).setText(title);
-                inflated.findViewById(R.id.btnBuyVip).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = ContainerActivity.getVipBuyIntent(null, "Likes");
-                        startActivityForResult(intent, ContainerActivity.INTENT_BUY_VIP_FRAGMENT);
-                    }
-                });
-                inflated.findViewById(R.id.btnRate).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        MenuFragment.selectFragment(FragmentId.F_DATING);
-                    }
-                });
-                ((ImageViewRemote) inflated.findViewById(R.id.ivOne))
-                        .setResourceSrc(CacheProfile.dating.sex == Static.GIRL ? R.drawable.likes_male_one : R.drawable.likes_female_one);
-                ((ImageViewRemote) inflated.findViewById(R.id.ivTwo))
-                        .setResourceSrc(CacheProfile.dating.sex == Static.GIRL ? R.drawable.likes_male_two : R.drawable.likes_female_two);
-                ((ImageViewRemote) inflated.findViewById(R.id.ivThree))
-                        .setResourceSrc(CacheProfile.dating.sex == Static.GIRL ? R.drawable.likes_male_three : R.drawable.likes_female_three);
-            } else {
-                ((ViewFlipper) inflated.findViewById(R.id.vfEmptyViews)).setDisplayedChild(0);
-                inflated.findViewById(R.id.btnStartRate).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        startActivity(ContainerActivity.getBuyingIntent("EmptyLikes"));
-                    }
-                });
-            }
         }
+    }
+
+    private void initEmptyScreenOnBlockedLikes(final View inflated, ViewFlipper viewFlipper, final Options.BlockSympathy blockSympathyOptions) {
+        // send stat to google analytics
+        sendBlockSympathyStatictics(blockSympathyOptions);
+        // set paid likes view
+        viewFlipper.setDisplayedChild(2);
+        View currentView = viewFlipper.getChildAt(2);
+        initAvatarImagesToEmptyView(currentView, blockSympathyOptions.showPhotos);
+        if (currentView != null) {
+            ((TextView) currentView.findViewById(R.id.tvText)).setText(blockSympathyOptions.text);
+            final Button btnBuy = (Button) currentView.findViewById(R.id.btnVipBuyCoins);
+            final ProgressBar progress = (ProgressBar) currentView.findViewById(R.id.prsLoading);
+            btnBuy.setText(blockSympathyOptions.buttonText);
+            btnBuy.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (CacheProfile.money >= blockSympathyOptions.price) {
+                        btnBuy.setVisibility(View.INVISIBLE);
+                        progress.setVisibility(View.VISIBLE);
+                        EasyTracker.getTracker().sendEvent(
+                                getTrackName(), "VipPaidSympathies." + blockSympathyOptions.group,
+                                "Buying", 1l
+                        );
+                        BuyLikesAccessRequest request = new BuyLikesAccessRequest(getActivity());
+                        request.callback(new SimpleApiHandler() {
+                            @Override
+                            public void success(IApiResponse response) {
+                                super.success(response);
+                                inflated.setVisibility(View.GONE);
+                                updateData(false, true);
+                            }
+
+                            @Override
+                            public void fail(int codeError, IApiResponse response) {
+                                super.fail(codeError, response);
+                                if (codeError == ErrorCodes.PAYMENT) {
+                                    Toast.makeText(getActivity(), R.string.not_enough_coins, Toast.LENGTH_LONG).show();
+                                    openBuyScreenOnBlockedLikes(blockSympathyOptions.group);
+                                }
+                            }
+
+                            @Override
+                            public void always(IApiResponse response) {
+                                super.always(response);
+                                btnBuy.setVisibility(View.VISIBLE);
+                                progress.setVisibility(View.GONE);
+                            }
+                        }).exec();
+                    } else {
+                        openBuyScreenOnBlockedLikes(blockSympathyOptions.group);
+                    }
+                }
+            });
+        }
+    }
+
+    private void openBuyScreenOnBlockedLikes(String group) {
+        EasyTracker.getTracker().sendEvent(
+                getTrackName(), "VipPaidSympathies." + group,
+                "OpenBuyingScreen", 1l
+        );
+        startActivity(ContainerActivity.getBuyingIntent("VipPaidSympathies." + group));
+    }
+
+    private void sendBlockSympathyStatictics(final Options.BlockSympathy blockSympathyOptions) {
+        new BackgroundThread() {
+            @Override
+            public void execute() {
+                SharedPreferences prefs = getActivity()
+                        .getSharedPreferences(Static.PREFERENCES_TAG_SHARED, Context.MODE_PRIVATE);
+                final long showsCount = prefs.getLong(Static.PREFERENCES_PAID_LIKES_COUNT, 1l);
+                if (showsCount > 1l) {
+                    EasyTracker.getTracker().sendEvent(
+                            getTrackName(), "VipPaidSympathies." + blockSympathyOptions.group,
+                            "ShownOnce", 1l
+                    );
+                } else {
+                    EasyTracker.getTracker().sendEvent(
+                            getTrackName(), "VipPaidSympathies." + blockSympathyOptions.group,
+                            "ShownMoreThanOnce", showsCount
+                    );
+                }
+                prefs.edit().putLong(Static.PREFERENCES_PAID_LIKES_COUNT, showsCount + 1l)
+                        .commit();
+            }
+        };
+    }
+
+    private void initAvatarImagesToEmptyView(View currentView) {
+        initAvatarImagesToEmptyView(currentView, true);
+    }
+
+    private void initAvatarImagesToEmptyView(View currentView, boolean visible) {
+        ImageViewRemote ivOne = (ImageViewRemote) currentView.findViewById(R.id.ivOne);
+        ImageViewRemote ivTwo = (ImageViewRemote) currentView.findViewById(R.id.ivTwo);
+        ImageViewRemote ivThree = (ImageViewRemote) currentView.findViewById(R.id.ivThree);
+        ivOne.setResourceSrc(CacheProfile.dating.sex == Static.GIRL ?
+                R.drawable.likes_male_one : R.drawable.likes_female_one);
+        ivTwo.setResourceSrc(CacheProfile.dating.sex == Static.GIRL ?
+                R.drawable.likes_male_two : R.drawable.likes_female_two);
+        ivThree.setResourceSrc(CacheProfile.dating.sex == Static.GIRL ?
+                R.drawable.likes_male_three : R.drawable.likes_female_three);
+        int visibility = visible ? View.VISIBLE : View.GONE;
+        ivOne.setVisibility(visibility);
+        ivTwo.setVisibility(visibility);
+        ivThree.setVisibility(visibility);
     }
 
     @Override
     protected int getEmptyFeedLayout() {
         return R.layout.layout_empty_likes;
+    }
+
+    @Override
+    protected boolean isForPremium() {
+        return true;
+    }
+
+    @Override
+    protected boolean isBlockOnClosing() {
+        return true;
     }
 
     @Override
