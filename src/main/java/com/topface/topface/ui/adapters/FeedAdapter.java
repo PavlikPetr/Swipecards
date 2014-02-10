4eb2835c5dfc62405b22a6b5d4ce93acc6be918d
Ilya Vorobiev <iovorobiev@gmail.com>
2014-02-10 15:41:24 +0400

    optimize imports

--- src/main/java/com/topface/topface/ui/adapters/FeedAdapter.java
+++ src/main/java/com/topface/topface/ui/adapters/FeedAdapter.java
@@ -10,7 +10,6 @@ import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 
