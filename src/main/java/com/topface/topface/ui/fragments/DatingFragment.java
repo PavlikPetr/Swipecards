ff09cc07abb1bc52dd2f5fe44a8ef7e88d60a129
Ilya Vorobiev <iovorobiev@gmail.com>
2014-02-12 12:51:17 +0400

    Исправил баг с монетами и восхищениями

--- src/main/java/com/topface/topface/ui/fragments/DatingFragment.java
+++ src/main/java/com/topface/topface/ui/fragments/DatingFragment.java
@@ -918,7 +918,6 @@ public class DatingFragment extends BaseFragment implements View.OnClickListener
     public void failRate() {
         unlockControls();
         if (moneyDecreased.get()) {
-            CacheProfile.money += CacheProfile.getOptions().priceAdmiration;
             moneyDecreased.set(false);
             updateResources();
         }
