8262e5a90f8602f24ee0503a04a2f8adfde26c23
Ilya Vorobiev <iovorobiev@gmail.com>
2014-02-20 14:02:43 +0400

    УБрал строку, добавил плюральные формы

--- src/main/java/com/topface/topface/utils/notifications/MessageStack.java
+++ src/main/java/com/topface/topface/utils/notifications/MessageStack.java
@@ -4,6 +4,7 @@ import android.text.Spannable;
 import android.text.SpannableString;
 
 import com.topface.topface.R;
+import com.topface.topface.utils.Utils;
 
 import java.util.LinkedList;
 
@@ -12,11 +13,6 @@ public class MessageStack extends LinkedList<Spannable>{
     public int getRestMessages() {
         return mRestMessages;
     }
-
-    public void setLastElem(String last) {
-        mLast = last;
-    }
-
     public void setRestMessages(int restMessages) {
         mRestMessages = restMessages;
     }
@@ -30,7 +26,7 @@ public class MessageStack extends LinkedList<Spannable>{
             if (mRestMessages > 0) {
                 removeLast();
             }
-            add(new SpannableString(String.format(mLast, mRestMessages)));
+            add(new SpannableString(Utils.getQuantityString(R.plurals.general_some_more, mRestMessages)));
         }
     }
 
@@ -42,12 +38,9 @@ public class MessageStack extends LinkedList<Spannable>{
         addAll(list);
     }
 
-    public MessageStack(String last) {
-        mLast = last;
-    }
+    public MessageStack() {}
 
     private int mRestMessages;
-    private String mLast;
     /**
      * Количество показываемых сообщений в нотификации. Когда пытаемся показать больше,
      * самое старое сообщение удаляется и вместо него ставится фраза
--- src/main/java/com/topface/topface/utils/notifications/UserNotificationManager.java
+++ src/main/java/com/topface/topface/utils/notifications/UserNotificationManager.java
@@ -25,6 +25,8 @@ import com.topface.topface.R;
 import com.topface.topface.utils.social.AuthToken;
 import com.topface.topface.utils.social.AuthorizationManager;
 
+import java.util.LinkedList;
+
 public class UserNotificationManager {
     private static UserNotificationManager mInstance;
     public static final int NOTIFICATION_ID = 1312; //Completely random number
@@ -204,10 +206,10 @@ public class UserNotificationManager {
             id = newNotificationId();
         }
 
-        MessageStack messagesStack = new MessageStack(mContext.getString(R.string.general_some_more));
+        MessageStack messagesStack = new MessageStack();
         if (intent.getIntExtra(Static.INTENT_REQUEST_KEY, -1) == ContainerActivity.INTENT_CHAT_FRAGMENT) {
             id = MESSAGES_ID;
-            messagesStack = saveMessageStack(title, message, user);
+            messagesStack = saveMessageStack(message, user);
         }
         UserNotification notification = new UserNotification(mContext);
         notification.setType(type);
@@ -224,11 +226,9 @@ public class UserNotificationManager {
         return id;
     }
 
-    private MessageStack saveMessageStack(String title, String message, User user) {
+    private MessageStack saveMessageStack(String message, User user) {
         UserConfig config = App.getUserConfig();
         MessageStack messagesStack = config.getNotificationMessagesStack();
-        messagesStack.setLastElem(mContext.getString(R.string.general_some_more));
-
         Spannable spanMessage = new SpannableString(String.format(mContext.getString(R.string.notification_message_format), user.name, message));
         spanMessage.setSpan(new StyleSpan(Typeface.BOLD), 0, user.name.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         messagesStack.addFirst(spanMessage);
