78b2d53ba65f8339810117eb61bcad93e79a90a9
Ilya Vorobiev <iovorobiev@gmail.com>
2014-02-21 17:07:27 +0400

    улучшаю адаптер

--- src/main/java/com/topface/topface/ui/adapters/PeopleCloseAdapter.java
+++ src/main/java/com/topface/topface/ui/adapters/PeopleCloseAdapter.java
@@ -1,4 +1,34 @@
 package com.topface.topface.ui.adapters;
 
+import android.content.Context;
+
 public class PeopleCloseAdapter extends FeedAdapter{
+    public PeopleCloseAdapter(Context context, Updater updateCallback) {
+        super(context, updateCallback);
+    }
+
+    @Override
+    protected int getItemLayout() {
+        return 0;
+    }
+
+    @Override
+    protected int getNewItemLayout() {
+        return 0;
+    }
+
+    @Override
+    protected int getVipItemLayout() {
+        return 0;
+    }
+
+    @Override
+    protected int getNewVipItemLayout() {
+        return 0;
+    }
+
+    @Override
+    public ILoaderRetrierCreator getLoaderRetrierCreator() {
+        return null;
+    }
 }
