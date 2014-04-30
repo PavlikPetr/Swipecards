ce1777423aee1e1ce4673bac366ba27691d135e8
Aleksandr Sakharuk <aleksandrsakharuk@gmail.com>
2014-04-30 14:47:11 +0400

    Move Switcher to separate class.

--- src/main/java/com/topface/topface/ui/edit/EditSwitcher.java
+++ src/main/java/com/topface/topface/ui/edit/EditSwitcher.java
@@ -1,17 +1,12 @@
 package com.topface.topface.ui.edit;
 
-import android.content.Context;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.text.TextUtils;
-import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.topface.topface.R;
+import com.topface.topface.ui.views.Switcher;
 
 public class EditSwitcher {
     private TextView mTitle;
@@ -77,57 +72,4 @@ public class EditSwitcher {
         setVisibility(!waiting);
     }
 
-    public static class Switcher extends Button {
-
-        private String mTextOn;
-        private String mTextOff;
-
-        private int mBackgroundOn;
-        private int mBackgrounOff;
-
-        private boolean mChecked;
-
-        public Switcher(Context context, AttributeSet attrs) {
-            super(context, attrs);
-            mTextOn = context.getString(R.string.settings_switch_on);
-            mTextOff = context.getString(R.string.settings_switch_off);
-            mBackgroundOn = R.drawable.edit_switch_on;
-            mBackgrounOff = R.drawable.edit_switch_off;
-            syncState();
-        }
-
-        @Override
-        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-            CharSequence defaultText = getText();
-            Paint meter = new Paint();
-            if (meter.measureText(mTextOn, 0, mTextOn.length()) > meter.measureText(mTextOff, 0, mTextOff.length())) {
-                setText(mTextOn);
-            } else {
-                setText(mTextOff);
-            }
-            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-            setText(defaultText);
-        }
-
-        public void setChecked(boolean checked) {
-            mChecked = checked;
-            syncState();
-        }
-
-        private void syncState() {
-            if (mChecked) {
-                setBackgroundResource(mBackgroundOn);
-                setTextColor(Color.parseColor("#FFFFFF"));
-                setText(mTextOn);
-            } else {
-                setBackgroundResource(mBackgrounOff);
-                setTextColor(Color.parseColor("#2F2F2F"));
-                setText(mTextOff);
-            }
-        }
-
-        public boolean isChecked() {
-            return mChecked;
-        }
-    }
 }
