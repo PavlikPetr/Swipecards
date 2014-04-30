e76f681498d1f9a3812d738e3497271155ba7ecf
kirussell <kirussellkirussell@gmail.com>
2014-04-30 15:11:00 +0400

    - Logs for Connection quality statistics

--- libs/AndroidStatistics/libs/Statistics/src/main/java/com/topface/statistics/NetworkDataDispatcher.java
+++ libs/AndroidStatistics/libs/Statistics/src/main/java/com/topface/statistics/NetworkDataDispatcher.java
@@ -11,21 +11,28 @@ public class NetworkDataDispatcher implements IDataDispatcher {
 
     private IHitDataBuilder mDataBuilder;
     private INetworkClient mNetworkClient;
+    private ILogger mLogger;
 
     public NetworkDataDispatcher(INetworkClient networkClient) {
         mNetworkClient = networkClient;
     }
 
     @Override
-    public void dispatchData(List<String> data) {
+    public void dispatchData(final List<String> data) {
         if (data != null && mNetworkClient != null && mDataBuilder != null) {
             mNetworkClient.sendRequest(mDataBuilder.build(data), new IRequestCallback() {
                 @Override
                 public void onSuccess() {
+                    if (mLogger != null) {
+                        mLogger.log("Data dispatched successfully (" + data.size() + ")");
+                    }
                 }
 
                 @Override
                 public void onFail() {
+                    if (mLogger != null) {
+                        mLogger.log("Data dispatch failed (" + data.size() + ")");
+                    }
                 }
 
                 @Override
@@ -38,6 +45,12 @@ public class NetworkDataDispatcher implements IDataDispatcher {
     @Override
     public NetworkDataDispatcher setDataBuilder(IHitDataBuilder dataBuilder) {
         mDataBuilder = dataBuilder;
+        return this;
+    }
+
+    @Override
+    public IDataDispatcher setLogger(ILogger logger) {
+        mLogger = logger;
         return this;
     }
 }
