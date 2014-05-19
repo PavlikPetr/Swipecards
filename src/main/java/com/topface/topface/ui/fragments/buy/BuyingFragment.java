08b2711e54c3a0d093012a2e8ff51c7288b1261e
kirussell <kirussellkirussell@gmail.com>
2014-05-19 16:38:52 +0400

    - Изменить текст на экране покупки монет и симпатий, если пользователю не хватает монет для разблокировки экрана симпатий #27997 FIX

--- src/main/java/com/topface/topface/ui/fragments/buy/BuyingFragment.java
+++ src/main/java/com/topface/topface/ui/fragments/buy/BuyingFragment.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import com.topface.topface.App;
 import com.topface.topface.R;
 import com.topface.topface.data.Products;
@@ -25,6 +26,7 @@ import com.topface.topface.utils.CacheProfile;
 import com.topface.topface.utils.CountersManager;
 import com.topface.topface.utils.Debug;
 import com.topface.topface.utils.offerwalls.OfferwallsManager;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.util.LinkedList;
@@ -141,11 +143,10 @@ public class BuyingFragment extends PaymentwallBuyingFragment {
             Bundle args = getArguments();
 
             int type = 0;
-            int coins = 0;
             int diff = 0;
             if (args != null) {
                 type = args.getInt(ARG_ITEM_TYPE);
-                coins = args.getInt(ARG_ITEM_PRICE);
+                int coins = args.getInt(ARG_ITEM_PRICE);
                 diff = coins - CacheProfile.money;
             }
             if (diff > 0) {
