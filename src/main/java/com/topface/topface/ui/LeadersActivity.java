625ea663f70a39e66ed3cfbee667e7c85d0b9aef
Maxim Bayutin <barmatolog@yandex.ru>
2014-11-13 16:22:29 +0300

    #31831: removed some old commented code

--- src/main/java/com/topface/topface/ui/LeadersActivity.java
+++ src/main/java/com/topface/topface/ui/LeadersActivity.java
@@ -57,7 +57,6 @@ public class LeadersActivity extends BaseFragmentActivity {
         usePhotos = new Photos();
         uselessPhotos = new Photos();
 
-//        mProgressBar = (ProgressBar) findViewById(R.id.loader);
         mGridView = (GridView) findViewById(R.id.useful_photos_grid);
         mUselessGridView = (GridView) findViewById(R.id.useless_photos_grid);
         mBuyButton = (Button) findViewById(R.id.btnLeadersBuy);
