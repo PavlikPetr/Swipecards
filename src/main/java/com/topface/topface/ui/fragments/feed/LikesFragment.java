e3d63ce872baae03c14333c01b7f9a14159ac0c7
PavlikPetr <petr.pavlik0205@gmail.com>
2014-11-26 18:53:53 +0300

    #33134: Открывается пустой экран симпатий вместо экрана блокировки, при переходе по пушу вернул обновление счетчиков лайков

--- src/main/java/com/topface/topface/ui/fragments/feed/LikesFragment.java
+++ src/main/java/com/topface/topface/ui/fragments/feed/LikesFragment.java
@@ -1,9 +1,12 @@
 package com.topface.topface.ui.fragments.feed;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -46,6 +49,32 @@ public class LikesFragment extends FeedFragment<FeedLike> {
 
     protected View mEmptyFeedView;
     private RateController mRateController;
+    private BroadcastReceiver mCountersReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            onCountersUpdated();
+        }
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(getActivity())
+                .registerReceiver(mCountersReceiver, new IntentFilter(CountersManager.UPDATE_COUNTERS));
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(getActivity())
+                .unregisterReceiver(mCountersReceiver);
+    }
+
+    protected void onCountersUpdated() {
+        if (mEmptyFeedView != null) {
+            initEmptyFeedView(mEmptyFeedView);
+        }
+    }
 
     @Override
     protected void init() {
@@ -113,6 +142,7 @@ public class LikesFragment extends FeedFragment<FeedLike> {
         if (mEmptyFeedView == null) mEmptyFeedView = inflated;
         ViewFlipper viewFlipper = (ViewFlipper) inflated.findViewById(R.id.vfEmptyViews);
         switch (errorCode) {
+            case ErrorCodes.RESULT_OK:
             case ErrorCodes.PREMIUM_ACCESS_ONLY:
                 initEmptyScreenOnLikesNeedVip(viewFlipper);
                 break;
