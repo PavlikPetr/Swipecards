15a108a96a73b92f6579c2b6f7221ba6e7cb46b7
PavlikPetr <petr.pavlik0205@gmail.com>
2014-11-27 15:41:06 +0300

    #33472 remove unused import

--- src/main/java/com/topface/topface/ui/adapters/FeedAdapter.java
+++ src/main/java/com/topface/topface/ui/adapters/FeedAdapter.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
