adf1aa81bc4f4c6cabfbd3a640bcff86176cf161
PavlikPetr <petr.pavlik0205@gmail.com>
2014-12-02 12:17:30 +0300

    #32095: add slices to unique statistic in push button "Buy VIP"

--- src/main/java/com/topface/topface/statistics/PushButtonVipUniqueStatistics.java
+++ src/main/java/com/topface/topface/statistics/PushButtonVipUniqueStatistics.java
@@ -1,5 +1,6 @@
 package com.topface.topface.statistics;
 
+import com.topface.statistics.android.Slices;
 import com.topface.statistics.android.StatisticsTracker;
 import com.topface.topface.App;
 import com.topface.topface.utils.CacheProfile;
@@ -7,21 +8,33 @@ import com.topface.topface.utils.CacheProfile;
 /**
  * Sending unique statistics about push button "Buy VIP".
  */
-public class PushButtonVipUniqueStatistics {
+public class PushButtonVipUniqueStatistics extends PushButtonVipStatistics {
 
     public static final String PUSH_BUTTON_VIP_UNIQUE = "mobile_push_button_vip_unique";
 
 
-    private static void send(String key, String unique) {
-        StatisticsTracker.getInstance().setContext(App.getContext()).sendUniqueEvent(key, 1, unique + "_" + key);
+    private static void sendStatistic(String key, Slices slices, String unique) {
+        StatisticsTracker.getInstance().setContext(App.getContext()).sendUniqueEvent(key, 1, slices, unique + "_" + key);
     }
 
+    @SuppressWarnings("UnusedDeclaration")
     public static void sendPushButtonVip(String unique) {
-        send(PUSH_BUTTON_VIP_UNIQUE, unique);
+        sendStatistic(PUSH_BUTTON_VIP_UNIQUE, null, unique);
     }
 
+    @SuppressWarnings("UnusedDeclaration")
     public static void sendPushButtonVip() {
-        send(PUSH_BUTTON_VIP_UNIQUE, Integer.toString(CacheProfile.getProfile().uid));
+        sendStatistic(PUSH_BUTTON_VIP_UNIQUE, null, Integer.toString(CacheProfile.getProfile().uid));
     }
+
+    public static void sendPushButtonVip(String button_type, String class_name, String from_screen_name) {
+        sendStatistic(PUSH_BUTTON_VIP_UNIQUE, generateSlices(button_type, class_name, from_screen_name), Integer.toString(CacheProfile.getProfile().uid));
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    public static void sendPushButtonVip(String unique, String button_type, String class_name, String from_screen_name) {
+        sendStatistic(PUSH_BUTTON_VIP_UNIQUE, generateSlices(button_type, class_name, from_screen_name), unique);
+    }
+
 
 }
