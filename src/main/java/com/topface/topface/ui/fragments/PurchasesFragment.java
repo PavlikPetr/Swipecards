4f934b751f5ed6fd446c0fd6f271dea460fa5eab
Aleksandr Sakharuk <aleksandrsakharuk@gmail.com>
2014-12-19 20:54:44 +0300

    Don't open topface offerwall for vip products.

--- src/main/java/com/topface/topface/ui/fragments/PurchasesFragment.java
+++ src/main/java/com/topface/topface/ui/fragments/PurchasesFragment.java
@@ -336,4 +336,8 @@ public class PurchasesFragment extends BaseFragment {
         return getString(R.string.buying_header_title);
     }
 
+    public boolean isVipProducts() {
+        return mIsVip;
+    }
+
 }
