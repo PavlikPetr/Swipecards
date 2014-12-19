ea9d3cc46a70a46daea77fd741fdc6c593b849db
Aleksandr Sakharuk <aleksandrsakharuk@gmail.com>
2014-12-19 18:23:13 +0300

    No left menu adapter update for old menu.

--- src/main/java/com/topface/topface/ui/NavigationActivity.java
+++ src/main/java/com/topface/topface/ui/NavigationActivity.java
@@ -456,8 +456,8 @@ public class NavigationActivity extends BaseFragmentActivity implements INavigat
     protected void onProfileUpdated() {
         initBonusCounterConfig();
         // возможно что содержимое меню поменялось, надо обновить
-        if (mMenuFragment != null) {
-            mMenuFragment.updataAdapter();
+        if (mMenuFragment != null && CacheProfile.getOptions().likesWithThreeTabs.isEnabled()) {
+            mMenuFragment.updateAdapter();
         }
         FloatBlock.resetActivityMap();
         mNotificationController.refreshNotificator();
