42098fd6efe9aac245f26910d1e420b47128bc94
Oleg <nikiti.oleg@gmail.com>
2014-12-22 16:09:06 +0300

    #33418 поправил подмодули

--- src/main/java/com/topface/topface/data/Photos.java
+++ src/main/java/com/topface/topface/data/Photos.java
@@ -75,7 +75,6 @@ public class Photos extends ArrayList<Photo> implements SerializableToJsonArray 
         return result;
     }
 
-    //todo
     public int getPhotoPosInArrayById(int photoId) {
         int pos;
         for (int i = 0; i < this.size(); i++) {
