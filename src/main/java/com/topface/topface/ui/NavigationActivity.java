2da2b849b04d0df07b3cc36a346d20ebe2678a9e
PavlikPetr <petr.pavlik0205@gmail.com>
2015-01-29 16:25:56 +0300

    #20935: created popup class
    (cherry picked from commit 154cfec9a8cbd4cd8e7e10b149c70e5bc19b9386)
    
    Conflicts:
    	src/main/java/com/topface/topface/ui/NavigationActivity.java
    	src/main/res/values-ru/strings.xml

--- src/main/java/com/topface/topface/ui/NavigationActivity.java
+++ src/main/java/com/topface/topface/ui/NavigationActivity.java
@@ -39,6 +39,7 @@ import com.topface.topface.requests.handlers.ApiHandler;
 import com.topface.topface.requests.handlers.ErrorCodes;
 import com.topface.topface.ui.dialogs.AbstractDialogFragment;
 import com.topface.topface.ui.dialogs.DatingLockPopup;
+import com.topface.topface.ui.dialogs.WrongTimePopup;
 import com.topface.topface.ui.fragments.MenuFragment;
 import com.topface.topface.ui.fragments.profile.DatingLockPopupAction;
 import com.topface.topface.ui.fragments.profile.OwnProfileFragment;
@@ -82,6 +83,8 @@ public class NavigationActivity extends BaseFragmentActivity implements INavigat
     public static final String FROM_AUTH = "com.topface.topface.AUTH";
     public static final String INTENT_EXIT = "EXIT";
     public static final String PAGE_SWITCH = "Page switch: ";
+
+    private WrongTimePopup mWrongTimePopup;
     private Intent mPendingNextIntent;
     ExternalLinkExecuter.OnExternalLinkListener mListener = new ExternalLinkExecuter.OnExternalLinkListener() {
         @Override
@@ -288,7 +291,8 @@ public class NavigationActivity extends BaseFragmentActivity implements INavigat
     @Override
     protected void onRegisterStartActions(StartActionsController startActionsController) {
         super.onRegisterStartActions(startActionsController);
-
+        mWrongTimePopup = new WrongTimePopup(NavigationActivity.this);
+        mWrongTimePopup.checkAndShowPopup();
         // actions after registration
         startActionsController.registerAction(createAfterRegistrationStartAction(AC_PRIORITY_HIGH));
         // promo popups
@@ -609,6 +613,11 @@ public class NavigationActivity extends BaseFragmentActivity implements INavigat
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == WrongTimePopup.ACTION_DATE_SETTINGS_INTENT_ID) {
+//            if (mWrongTimePopup != null) {
+//                mWrongTimePopup.checkAndShowPopup();
+//            }
+//        }
         AbstractDialogFragment currentPopup = mPopupManager.getCurrentDialog();
         if (currentPopup != null) {
             currentPopup.onActivityResult(requestCode, resultCode, data);
