734fd892dcd941f653612fceba61cf167b0ee700
Maxim Bayutin <barmatolog@yandex.ru>
2015-02-24 16:44:38 +0300

    #36166: undefined redirect now has name

--- src/main/java/com/topface/topface/statistics/RedirectStatistics.java
+++ src/main/java/com/topface/topface/statistics/RedirectStatistics.java
@@ -12,11 +12,14 @@ import com.topface.statistics.android.StatisticsTracker;
 public class RedirectStatistics {
 
     private static final String TF_MOBILE_REDIRECT = "mobile_tf_redirect";
+    private static final String TF_MOBILE_REDIRECT_UNDEFINED = "undefined";
 
     public static void send(String host) {
-        if (!TextUtils.isEmpty(host)) {
-            StatisticsTracker.getInstance()
-                    .sendEvent(TF_MOBILE_REDIRECT, 1, new Slices().putSlice(TfStatConsts.val, host));
+        if (TextUtils.isEmpty(host)) {
+            host = TF_MOBILE_REDIRECT_UNDEFINED;
         }
+        StatisticsTracker.getInstance()
+                .sendEvent(TF_MOBILE_REDIRECT, 1, new Slices().putSlice(TfStatConsts.val, host));
+
     }
 }
