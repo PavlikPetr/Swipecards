8255506726d25d5e0e93b02d707b65aaea5c2498
Maxim Bayutin <barmatolog@yandex.ru>
2015-02-24 16:22:24 +0300

    #36166: redirection statistic added

---
+++ src/main/java/com/topface/topface/statistics/RedirectStatistics.java
@@ -0,0 +1,22 @@
+package com.topface.topface.statistics;
+
+import android.text.TextUtils;
+
+import com.topface.statistics.android.Slices;
+import com.topface.statistics.android.StatisticsTracker;
+
+/**
+ * Created by mbautin on 24.02.15.
+ * Sends redirection statistics from sheme topface://mobvk for example
+ */
+public class RedirectStatistics {
+
+    private static final String TF_MOBILE_REDIRECT = "mobile_tf_redirect";
+
+    public static void send(String host) {
+        if (!TextUtils.isEmpty(host)) {
+            StatisticsTracker.getInstance()
+                    .sendEvent(TF_MOBILE_REDIRECT, 1, new Slices().putSlice(TfStatConsts.val, host));
+        }
+    }
+}
