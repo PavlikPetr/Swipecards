ef4ed5c76f39e327707be6382639d894b8ebfdb9
Oleg <nikiti.oleg@gmail.com>
2015-02-27 17:50:42 +0300

    #36268: Добавлена обработка ошибки UNSUPPORTED_VERSION

--- src/main/java/com/topface/topface/ui/fragments/BaseAuthFragment.java
+++ src/main/java/com/topface/topface/ui/fragments/BaseAuthFragment.java
@@ -183,12 +183,7 @@ public abstract class BaseAuthFragment extends BaseFragment {
                 showRetryView(getString(R.string.general_maintenance), strBuilder.toString(), request);
                 break;
             case ErrorCodes.CODE_OLD_APPLICATION_VERSION:
-                fillRetryView(getString(R.string.general_version_not_supported), new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Utils.goToMarket(getActivity());
-                    }
-                }, getString(R.string.popup_version_update));
+                Utils.startOldVersionPopup(getActivity());
                 break;
             default:
                 showRetryView(getString(R.string.general_data_error), strBuilder.toString(), request);
