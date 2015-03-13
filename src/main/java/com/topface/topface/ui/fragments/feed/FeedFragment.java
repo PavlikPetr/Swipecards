0d18a15c5784d57c19c4a92433b30550fee0ea0a
Oleg <nikiti.oleg@gmail.com>
2015-03-13 20:07:26 +0300

    #28293: После удаления итемов в черном списке отображаем "Ваш черный список пуст"

--- src/main/java/com/topface/topface/ui/fragments/feed/FeedFragment.java
+++ src/main/java/com/topface/topface/ui/fragments/feed/FeedFragment.java
@@ -558,10 +558,6 @@ public abstract class FeedFragment<T extends FeedItem> extends BaseFragment
             public void success(IApiResponse response) {
                 if (isAdded()) {
                     getListAdapter().removeItems(items);
-                    if (getListAdapter().getData().size() == 0) {
-                        mListView.setVisibility(View.INVISIBLE);
-                        onEmptyFeed();
-                    }
                 }
             }
 
