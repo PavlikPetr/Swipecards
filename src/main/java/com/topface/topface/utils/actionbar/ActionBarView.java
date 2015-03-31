b5905b64d1f63c46a4b50ad886ca4fa1a51fffcb
Oleg <nikiti.oleg@gmail.com>
2015-03-31 17:32:57 +0300

    action bar view npe on click

--- src/main/java/com/topface/topface/utils/actionbar/ActionBarView.java
+++ src/main/java/com/topface/topface/utils/actionbar/ActionBarView.java
@@ -100,7 +100,9 @@ public class ActionBarView implements View.OnClickListener {
                     if (noBackStack) {
                         intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
                     }
-                    mActivity.startActivity(intent);
+                    if (intent != null) {
+                        mActivity.startActivity(intent);
+                    }
                     mActivity.finish();
                 }
             } else if (mActivity instanceof BanActivity) {
