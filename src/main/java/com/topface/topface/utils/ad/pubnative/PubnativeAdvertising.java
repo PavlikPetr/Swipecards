284e09c6fa23c349ece472381c545e6857f6fe9d
Oleg <nikiti.oleg@gmail.com>
2015-04-02 18:09:47 +0300

    new constant

--- src/main/java/com/topface/topface/utils/ad/pubnative/PubnativeAdvertising.java
+++ src/main/java/com/topface/topface/utils/ad/pubnative/PubnativeAdvertising.java
@@ -22,7 +22,7 @@ import java.util.List;
 public class PubnativeAdvertising extends Advertising {
 
     private static final String REQUEST = "http://api.pubnative.net/api/partner/v2/promotions/native?";
-    private static final String ODNOKLASSNIKI = "ok";
+    private static final String OK = "ok";
 
     private PubnativeInfo mPubnativeInfo;
 
@@ -46,7 +46,7 @@ public class PubnativeAdvertising extends Advertising {
         try {
             PubnativeResponse pubnativeResponse = JsonUtils.fromJson(response, PubnativeResponse.class);
             if (pubnativeResponse != null
-                    && TextUtils.equals(pubnativeResponse.getStatus(), ODNOKLASSNIKI)) {
+                    && TextUtils.equals(pubnativeResponse.getStatus(), OK)) {
                 PubnativeAd[] ads = pubnativeResponse.getAds();
                 if (ads != null) {
                     for (PubnativeAd ad : ads) {
