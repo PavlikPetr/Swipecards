594a9bc9cf5d529cb4e4b5be98540eab5b877820
wen0m <karpushov@topface.com>
2015-04-07 14:53:11 +0300

    #37425 remove experiment from chat on dating screen

--- src/main/java/com/topface/topface/data/experiments/InstantMessageFromSearch.java
+++ src/main/java/com/topface/topface/data/experiments/InstantMessageFromSearch.java
@@ -1,9 +1,26 @@
 package com.topface.topface.data.experiments;
 
-public class InstantMessageFromSearch extends BaseExperimentWithText {
+import org.json.JSONObject;
 
-    @Override
-    protected String getOptionsKey() {
-        return "instantMessageFromSearch";
+public class InstantMessageFromSearch {
+
+    protected static final String KEY_TEXT = "text";
+    protected static final String INSTANT_MSG = "instantMessageFromSearch";
+    private String mText;
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public JSONObject init(JSONObject response) {
+        JSONObject source = response.optJSONObject(INSTANT_MSG);;
+        if (source != null) {
+            setText(source.optString(KEY_TEXT, ""));
+        }
+        return source;
     }
 }
