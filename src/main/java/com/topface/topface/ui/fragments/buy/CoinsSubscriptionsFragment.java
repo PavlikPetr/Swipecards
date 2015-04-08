2ec7840d0d744777067a355c43a6434c7cf27798
PavlikPetr <petr.pavlik0205@gmail.com>
2015-04-08 19:01:00 +0300

    #37516: buying screen on light design

--- src/main/java/com/topface/topface/ui/fragments/buy/CoinsSubscriptionsFragment.java
+++ src/main/java/com/topface/topface/ui/fragments/buy/CoinsSubscriptionsFragment.java
@@ -2,7 +2,10 @@ package com.topface.topface.ui.fragments.buy;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.view.LayoutInflater;
@@ -36,6 +39,24 @@ import java.util.List;
 public class CoinsSubscriptionsFragment extends OpenIabFragment {
     private LinearLayout mContainer;
     private List<View> mButtonsViews = new ArrayList<>();
+    private TextView mResourceInfo;
+    private String mResourceInfoText;
+    private int mResourceInfoVisibility;
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent != null) {
+                if (intent.hasExtra(ARG_RESOURCE_INFO_TEXT)) {
+                    mResourceInfoText = intent.getExtras().getString(ARG_RESOURCE_INFO_TEXT);
+                    setResourceInfoText();
+                }
+                if (intent.hasExtra(ARG_RESOURCE_INFO_VISIBILITY)) {
+                    mResourceInfoVisibility = intent.getExtras().getInt(ARG_RESOURCE_INFO_VISIBILITY);
+                    setResourceInfoVisibility();
+                }
+            }
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,10 +65,25 @@ public class CoinsSubscriptionsFragment extends OpenIabFragment {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(mReceiver, new IntentFilter(OpenIabFragment.UPDATE_RESOURCE_INFO));
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(getActivity()).unregisterReceiver(mReceiver);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         @SuppressLint("InflateParams") View root = inflater.inflate(R.layout.fragment_coins_subscription, null);
         mContainer = (LinearLayout) root.findViewById(R.id.loContainer);
+        mResourceInfo = (TextView) root.findViewById(R.id.payReasonFragmentCoinsSubsription);
+        setResourceInfoText();
+        setResourceInfoVisibility();
         Products products = getProducts();
         if (products != null) {
             CoinsSubscriptionInfo info = products.info.coinsSubscription;
@@ -148,6 +184,18 @@ public class CoinsSubscriptionsFragment extends OpenIabFragment {
                             .sendBroadcast(new Intent(Products.INTENT_UPDATE_PRODUCTS));
                 }
             });
+        }
+    }
+
+    private void setResourceInfoText() {
+        if (mResourceInfo != null) {
+            mResourceInfo.setText(mResourceInfoText);
+        }
+    }
+
+    private void setResourceInfoVisibility() {
+        if (mResourceInfo != null) {
+            mResourceInfo.setVisibility(mResourceInfoVisibility);
         }
     }
 
