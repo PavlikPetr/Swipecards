5dd2c32a9c20a0907b32e51fdfd3ca2df63bd7aa
Ruslan Kim <kirussellkirussell@gmail.com>
2015-04-19 18:33:27 +0300

    - fixes null parent intent for photoBlogFeed

--- src/main/java/com/topface/topface/data/experiments/FeedScreensIntent.java
+++ src/main/java/com/topface/topface/data/experiments/FeedScreensIntent.java
@@ -2,6 +2,7 @@ package com.topface.topface.data.experiments;
 
 import android.content.Intent;
 
+import com.topface.topface.ui.fragments.BaseFragment;
 import com.topface.topface.ui.fragments.feed.DialogsFragment;
 import com.topface.topface.ui.fragments.feed.PhotoBlogFragment;
 import com.topface.topface.ui.fragments.feed.TabbedFeedFragment;
@@ -12,12 +13,17 @@ import static com.topface.topface.ui.fragments.BaseFragment.FragmentId.TABBED_DI
 
 public class FeedScreensIntent {
     public static void equipMessageAllIntent(Intent intent) {
-        intent.putExtra(GCMUtils.NEXT_INTENT, TABBED_DIALOGS);
-        intent.putExtra(TabbedFeedFragment.EXTRA_OPEN_PAGE, DialogsFragment.class.getName());
+        equipFeedIntent(intent, TABBED_DIALOGS, DialogsFragment.class.getName());
     }
 
     public static void equipPhotoFeedIntent(Intent intent) {
-        intent.putExtra(GCMUtils.NEXT_INTENT, PHOTO_BLOG);
-        intent.putExtra(TabbedFeedFragment.EXTRA_OPEN_PAGE, PhotoBlogFragment.class.getName());
+        equipFeedIntent(intent, PHOTO_BLOG, PhotoBlogFragment.class.getName());
+    }
+
+    private static void equipFeedIntent(Intent intent, BaseFragment.FragmentId feedId, String pageName) {
+        if (intent != null) {
+            intent.putExtra(GCMUtils.NEXT_INTENT, feedId);
+            intent.putExtra(TabbedFeedFragment.EXTRA_OPEN_PAGE, pageName);
+        }
     }
 }
