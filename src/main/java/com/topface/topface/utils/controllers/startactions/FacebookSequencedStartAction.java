accdec3c50a0dfe2a33c8e742600dd1884f8a3bc
Oleg <nikiti.oleg@gmail.com>
2015-05-06 16:55:10 +0300

    #38230 починил падение SequencedStartAction с ConcurrentModificationException, добавил тест для проверки этого дела.

--- src/main/java/com/topface/topface/utils/controllers/startactions/FacebookSequencedStartAction.java
+++ src/main/java/com/topface/topface/utils/controllers/startactions/FacebookSequencedStartAction.java
@@ -1,15 +1,13 @@
 package com.topface.topface.utils.controllers.startactions;
 
-import android.app.Activity;
-
 import com.topface.topface.utils.controllers.SequencedStartAction;
 import com.topface.topface.utils.social.AuthToken;
 
 public class FacebookSequencedStartAction extends SequencedStartAction {
 
 
-    public FacebookSequencedStartAction(Activity activity, int priority) {
-        super(activity, priority);
+    public FacebookSequencedStartAction(IActivityEmulator mActivityEmulator, int priority) {
+        super(mActivityEmulator, priority);
     }
 
     @Override
