f8c05cc5b2c7d6456cc9fe6a116e135855519a0f
PavlikPetr <petr.pavlik0205@gmail.com>
2015-05-08 10:37:05 +0300

    #38097: changed dating and chat input

--- src/main/java/com/topface/topface/utils/controllers/DatingInstantMessageController.java
+++ src/main/java/com/topface/topface/utils/controllers/DatingInstantMessageController.java
@@ -225,7 +225,7 @@ public class DatingInstantMessageController {
 
     public void openChat(FragmentActivity activity, SearchUser user) {
         if (user != null) {
-            Intent intent = ChatActivity.createIntent(user.id,user.getNameAndAge(),user.city.name,null,false);
+            Intent intent = ChatActivity.createIntent(user.id, user.getNameAndAge(), user.city.name, null, false);
             activity.startActivityForResult(intent, ChatActivity.INTENT_CHAT);
             EasyTracker.sendEvent("Dating", "Additional", "Chat", 1L);
         }
