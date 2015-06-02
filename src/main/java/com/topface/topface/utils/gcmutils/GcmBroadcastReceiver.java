614b315e3cd08afbc54fc1ae0a2e7df8ab07b519
Ruslan Kim <kirussellkirussell@gmail.com>
2015-06-02 12:29:53 +0300

    - fixes gcm broadcasts exceptions with not orderedBroadcast

--- src/main/java/com/topface/topface/utils/gcmutils/GcmBroadcastReceiver.java
+++ src/main/java/com/topface/topface/utils/gcmutils/GcmBroadcastReceiver.java
@@ -16,6 +16,8 @@ public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
                 GcmIntentService.class.getName());
         // Start the service, keeping the device awake while it is launching.
         startWakefulService(context, (intent.setComponent(comp)));
-        setResultCode(Activity.RESULT_OK);
+        if(isOrderedBroadcast()) {
+            setResultCode(Activity.RESULT_OK);
+        }
     }
 }
