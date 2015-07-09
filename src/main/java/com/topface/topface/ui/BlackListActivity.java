33dca661b7bd1d783bc1b8dc9f0e4a861ae9f0cb
Oleg <nikiti.oleg@gmail.com>
2015-07-09 14:19:02 +0300

    #39852: титул черного списка отображается как нужно

--- src/main/java/com/topface/topface/ui/BlackListActivity.java
+++ src/main/java/com/topface/topface/ui/BlackListActivity.java
@@ -1,32 +1,18 @@
 package com.topface.topface.ui;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 
-import com.topface.topface.R;
 import com.topface.topface.ui.fragments.feed.BlackListFragment;
 
 
-public class BlackListActivity extends BaseFragmentActivity {
+public class BlackListActivity extends SingleFragmentActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Fragment fragment = getSupportFragmentManager().findFragmentByTag(BlackListFragment.class.getSimpleName());
-        FragmentTransaction beginTransaction = getSupportFragmentManager().beginTransaction();
-        if (fragment == null) {
-            beginTransaction.add(R.id.blackList, new BlackListFragment(), BlackListFragment.class.getSimpleName())
-                    .commit();
-        } else {
-            beginTransaction.replace(R.id.blackList, fragment, BlackListFragment.class.getSimpleName())
-                    .commit();
-        }
-
+    protected String getFragmentTag() {
+        return BlackListFragment.class.getSimpleName();
     }
-
     @Override
-    protected int getContentLayout() {
-        return R.layout.ac_black_list_wrapper;
+    protected Fragment createFragment() {
+        return new BlackListFragment();
     }
 }
