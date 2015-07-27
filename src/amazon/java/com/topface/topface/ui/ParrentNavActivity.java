8f036162557adf7e9335252502e16f767915c1f7
PavlikPetr <petr.pavlik0205@gmail.com>
2015-07-27 12:30:54 +0300

    #39903: new flavour i-free

---
+++ src/amazon/java/com/topface/topface/ui/ParrentNavActivity.java
@@ -0,0 +1,14 @@
+package com.topface.topface.ui;
+
+/**
+ * Created by ppetr on 20.07.15.
+ * empty parrent for NavigationActivity (it needs in i-free flavour)
+ */
+public abstract class ParrentNavActivity extends BaseFragmentActivity {
+    @Override
+    protected int getContentLayout() {
+        return getContentLayoutId();
+    }
+
+    protected abstract int getContentLayoutId();
+}
