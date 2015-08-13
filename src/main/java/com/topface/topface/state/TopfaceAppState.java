ba61b3efa20795055570116f4f6facd3b137e9c4
Ruslan Kim <kirussellkirussell@gmail.com>
2015-08-13 15:17:51 +0300

    - makes AppState to be observed on Main Thread

--- src/main/java/com/topface/topface/state/TopfaceAppState.java
+++ src/main/java/com/topface/topface/state/TopfaceAppState.java
@@ -2,6 +2,9 @@ package com.topface.topface.state;
 
 import com.topface.topface.data.BalanceData;
 
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+
 /**
  * Created by ppetr on 15.06.15.
  * child AppState class for Topface application
@@ -17,5 +20,10 @@ public class TopfaceAppState extends AppState {
 
     public BalanceData getBalance() {
         return getNotNullData(new BalanceData(false, 0, 0));
+    }
+
+    public <T> Observable<T> getObservable(Class<T> dataClass) {
+        return super.getObservable(dataClass)
+                .observeOn(AndroidSchedulers.mainThread());
     }
 }
