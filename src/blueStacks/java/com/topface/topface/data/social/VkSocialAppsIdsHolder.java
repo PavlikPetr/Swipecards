13b1c5865701fe97ebac92141199c03c33880179
PavlikPetr <petr.pavlik0205@gmail.com>
2015-08-20 12:44:04 +0300

    #40629: change vkId, added new keystores (topfaceBluestacks, topfaceIFree)

--- src/blueStacks/java/com/topface/topface/data/social/VkSocialAppsIdsHolder.java
+++ src/blueStacks/java/com/topface/topface/data/social/VkSocialAppsIdsHolder.java
@@ -2,19 +2,16 @@ package com.topface.topface.data.social;
 
 import java.util.ArrayList;
 
-/**
- * Created by kirussell on 31/07/15.
- */
 class VkSocialAppsIdsHolder {
 
     /**
      * {String} VK_ID - идентификатор приложения в vk
      */
-    static final String VK_ID = "5015934";
+    static final String VK_ID = "5029543";
     static final ArrayList<String> ALLOWED_VK_IDS = new ArrayList<>(1);
 
     static {
-        ALLOWED_VK_IDS.add("5015934");
+        ALLOWED_VK_IDS.add("5029543");
     }
 
     public static String checkAllowedVkId(String serverId) {
