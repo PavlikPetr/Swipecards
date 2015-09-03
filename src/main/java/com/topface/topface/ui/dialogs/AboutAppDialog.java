21fd0929e31f63751def98b36e08061e716e53ba
PavlikPetr <petr.pavlik0205@gmail.com>
2015-09-03 15:06:13 +0300

    #14217: merge develop

--- src/main/java/com/topface/topface/ui/dialogs/AboutAppDialog.java
+++ src/main/java/com/topface/topface/ui/dialogs/AboutAppDialog.java
@@ -6,7 +6,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.text.SpannableString;
@@ -83,7 +82,7 @@ public class AboutAppDialog extends TrackedDialogFragment {
             public void onClick(View v) {
                 Intent i = Utils.getIntentToOpenUrl(CacheProfile.getOptions().aboutApp.url);
                 if (i != null) {
-                    mContext.startActivity(i);
+                    getActivity().startActivity(i);
                 }
             }
         });
--- src/main/java/com/topface/topface/ui/fragments/SettingsFragment.java
+++ src/main/java/com/topface/topface/ui/fragments/SettingsFragment.java
@@ -1,6 +1,7 @@
 package com.topface.topface.ui.fragments;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
