118f5e7cc8a74b4abd60f203679533c9702a6881
Ruslan Kim <kirussellkirussell@gmail.com>
2015-09-14 13:42:12 +0300

    - ifree vk id from AndroidManifest.xml

--- src/ifree/java/com/topface/topface/data/social/VkSocialAppsIdsHolder.java
+++ src/ifree/java/com/topface/topface/data/social/VkSocialAppsIdsHolder.java
@@ -1,16 +1,28 @@
 package com.topface.topface.data.social;
 
-import java.util.ArrayList;
+import android.content.pm.PackageManager;
+
+import com.topface.framework.utils.Debug;
+import com.topface.topface.App;
 
 /**
  * Created by kirussell on 31/07/15.
  */
 class VkSocialAppsIdsHolder {
 
-    /**
-     * {String} VK_ID - идентификатор приложения в vk
-     */
-    static final String VK_ID = "5015934";
+    static final String VK_ID = getVkId();
+
+    private static String getVkId()  {
+        String res = "5015934";
+        try {
+            res = String.valueOf(App.getContext().getPackageManager()
+                    .getApplicationInfo(App.getContext().getPackageName(), PackageManager.GET_META_DATA)
+                    .metaData.get("vk_id"));
+        } catch (PackageManager.NameNotFoundException e) {
+            Debug.log(e.toString());
+        }
+        return res;
+    }
 
     public static String checkAllowedVkId(String serverId) {
         //ignore server ids for Ifree
