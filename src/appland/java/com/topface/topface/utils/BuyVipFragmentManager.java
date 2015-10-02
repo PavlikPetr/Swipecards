ca484870f0dd0a37705b52b32c5fe72be09b0987
PavlikPetr <petr.pavlik0205@gmail.com>
2015-10-02 12:05:28 +0300

    #41974: new flavour Appland

---
+++ src/appland/java/com/topface/topface/utils/BuyVipFragmentManager.java
@@ -0,0 +1,21 @@
+package com.topface.topface.utils;
+
+
+import com.topface.topface.data.PaymentWallProducts;
+import com.topface.topface.ui.fragments.buy.VipBuyFragment;
+import com.topface.topface.ui.fragments.buy.VipPaymentWallBuyFragment;
+
+/**
+ * Created by ppetr on 23.07.15.
+ * hold method to return right class buying vip fragment
+ */
+public class BuyVipFragmentManager {
+
+    public static String getClassName() {
+        return VipPaymentWallBuyFragment.class.getName();
+    }
+
+    public static VipBuyFragment getVipInstance(boolean needActionBar, String from, String text) {
+        return VipBuyFragment.newInstance(needActionBar, from, text);
+    }
+}
