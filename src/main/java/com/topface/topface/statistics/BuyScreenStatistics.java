11618eb82be7edf650b04da2b5ecf62393dc2253
PavlikPetr <petr.pavlik0205@gmail.com>
2016-03-29 17:51:53 +0300

    new vip buy screen statistics

---
+++ src/main/java/com/topface/topface/statistics/BuyScreenStatistics.java
@@ -0,0 +1,45 @@
+package com.topface.topface.statistics;
+
+import com.topface.statistics.android.Slices;
+import com.topface.statistics.android.StatisticsTracker;
+import com.topface.topface.App;
+import com.topface.topface.utils.CacheProfile;
+
+/**
+ * Promo popup show unique statistics
+ */
+public class BuyScreenStatistics {
+
+    public static final String BUY_SCREEN_SHOW = "buy_screen_show";
+    public static final String TAG = "tag";
+    public static final String PLC = "plc";
+
+    private static void sendUniqueStatistics(String screenName, Slices slices) {
+        StatisticsTracker
+                .getInstance()
+                .setContext(App.getContext())
+                .sendUniqueEvent(BUY_SCREEN_SHOW, 1, slices, Integer.toString(CacheProfile.getProfile().uid) + "_" + screenName);
+    }
+
+    private static void sendRegularStatistics(Slices slices) {
+        StatisticsTracker.getInstance()
+                .sendEvent(BUY_SCREEN_SHOW, 1, slices);
+    }
+
+    public static void buyScreenShowSendStatistics(String screenName, String screenVersion) {
+        if (screenName != null) {
+            screenName = screenName.toLowerCase();
+        } else {
+            return;
+        }
+        Slices slices = generateSlices(screenVersion, screenName);
+        sendUniqueStatistics(screenName, slices);
+        sendRegularStatistics(slices);
+    }
+
+    private static Slices generateSlices(String version, String screenName) {
+        return new Slices()
+                .putSlice(TAG, version)
+                .putSlice(PLC, screenName);
+    }
+}
