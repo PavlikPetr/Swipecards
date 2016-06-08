de45d7e6bf92554f5fd7bdd506d3c8083309151e
Tiberal <nikiti.oleg@gmail.com>
2016-06-08 17:48:02 +0300

    #49774 refactor

--- src/main/java/com/topface/topface/ui/fragments/profile/OwnProfileFragment.java
+++ src/main/java/com/topface/topface/ui/fragments/profile/OwnProfileFragment.java
@@ -7,9 +7,7 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.MenuItemCompat;
 import android.view.LayoutInflater;
@@ -48,15 +46,21 @@ public class OwnProfileFragment extends OwnAvatarFragment {
     private boolean mIsPhotoAsked;
     private static final String TAKE_PHOTO_DIALOG_SHOWN = "dialog_shown";
     private MenuItem mBarAvatar;
-    private Handler mHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            AddPhotoHelper.handlePhotoMessage(msg);
-        }
-    };
+    private Handler mHandler;
 
     public static OwnProfileFragment newInstance() {
         return new OwnProfileFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        mHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                AddPhotoHelper.handlePhotoMessage(msg);
+            }
+        };
+        super.onCreate(savedInstanceState);
     }
 
     @Override
