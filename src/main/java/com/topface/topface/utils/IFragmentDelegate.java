93d0365e15274675a0cbdcca1779aeef4a5f9b65
lnfn <lnfn@ya.ru>
2016-06-28 17:55:44 +0300

    #49839 Добавлен метод destroyController в RateController для обнуленя ссылки на Context

--- src/main/java/com/topface/topface/utils/IFragmentDelegate.java
+++ src/main/java/com/topface/topface/utils/IFragmentDelegate.java
@@ -1,11 +1,8 @@
 package com.topface.topface.utils;
 
-import android.app.Activity;
 import android.support.v4.app.FragmentActivity;
 
-/**
- * Created by user on 28.06.16.
- */
+
 public interface IFragmentDelegate {
     FragmentActivity getActivity();
 }
