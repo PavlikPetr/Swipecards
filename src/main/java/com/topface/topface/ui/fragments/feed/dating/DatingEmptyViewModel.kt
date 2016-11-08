a4f9eea3b42fa9be3eb91a5d0281a35489d26c40
garastard <mb@blackbeautystudio.com>
2016-11-08 11:10:55 +0300

    Add processing for buttons

--- src/main/java/com/topface/topface/ui/fragments/feed/dating/DatingEmptyViewModel.kt
+++ src/main/java/com/topface/topface/ui/fragments/feed/dating/DatingEmptyViewModel.kt
@@ -1,7 +1,22 @@
 package com.topface.topface.ui.fragments.feed.dating
 
+import android.view.View
+import com.topface.topface.R
+import kotlinx.android.synthetic.main.layout_empty_dating.*
+
 /**
  * Created by mbulgakov on 07.11.16.
  */
-enum class DatingEmptyViewModel {
+class DatingEmptyViewModel {
+
+    fun onButtonClick(v: View) {
+        when (v.id) {
+            R.id.btnChangeFilterDating -> doSomething()
+            R.id.btnClearFilterDating -> doSomething()
+        }
+    }
+
+    private fun doSomething() {
+
+    }
 }
