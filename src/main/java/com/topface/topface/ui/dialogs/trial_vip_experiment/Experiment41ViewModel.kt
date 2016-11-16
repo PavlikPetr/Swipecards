ac3af774d0ffecb72c5089ead66ad2108d2c9c95
siberia87 <s.salnikov@topface.com>
2016-11-16 15:31:42 +0300

    сделал загрузку заглушки для аватарки

--- src/main/java/com/topface/topface/ui/dialogs/trial_vip_experiment/Experiment41ViewModel.kt
+++ src/main/java/com/topface/topface/ui/dialogs/trial_vip_experiment/Experiment41ViewModel.kt
@@ -2,8 +2,11 @@ package com.topface.topface.ui.dialogs.trial_vip_experiment
 
 import android.databinding.ObservableField
 import com.topface.topface.App
+import com.topface.topface.R
+import com.topface.topface.data.Profile
 import com.topface.topface.databinding.LayoutExperiment41Binding
 import com.topface.topface.viewModels.BaseViewModel
+import org.jetbrains.anko.imageResource
 
 /**
  * модель для эксперимента 4_1
@@ -11,5 +14,12 @@ import com.topface.topface.viewModels.BaseViewModel
  */
 class Experiment41ViewModel(binding: LayoutExperiment41Binding) :
         BaseViewModel<LayoutExperiment41Binding>(binding) {
-    val userAvatar = ObservableField(App.get().profile.photo.defaultLink)
+    val userAvatar = ObservableField(App.get().profile.photo.defaultLink ?: setFakeAvatar())
+
+    fun setFakeAvatar() {
+        binding.userAvatar.imageResource = when {
+            (App.get().profile.sex == Profile.GIRL) ->  R.drawable.upload_photo_female
+            else -> R.drawable.upload_photo_male
+        }
+    }
 }
