9ec6a123e1f88d517af1b49fc41d537fb515cc44
siberia87 <s.salnikov@topface.com>
2016-11-18 14:33:55 +0300

    random img

--- src/main/java/com/topface/topface/ui/dialogs/trial_vip_experiment/Experiment41ViewModel.kt
+++ src/main/java/com/topface/topface/ui/dialogs/trial_vip_experiment/Experiment41ViewModel.kt
@@ -10,6 +10,7 @@ import com.topface.topface.state.TopfaceAppState
 import com.topface.topface.utils.Utils
 import com.topface.topface.viewModels.BaseViewModel
 import rx.Subscription
+import java.util.*
 import javax.inject.Inject
 
 /**
@@ -19,14 +20,30 @@ import javax.inject.Inject
 class Experiment41ViewModel(binding: LayoutExperiment41Binding) :
         BaseViewModel<LayoutExperiment41Binding>(binding) {
 
-    val userAvatar: ObservableField<String> = ObservableField(Utils.getLocalResUrl(R.drawable.upload_photo_female))
+    companion object {
+        const val photoCount = 2
+    }
+
+    val userAvatar: ObservableField<String> = ObservableField()
+    val randomLeftPhoto: ObservableField<Int> = ObservableField()
+    val randomRightPhoto: ObservableField<Int> = ObservableField()
     var profileSubscription: Subscription
     @Inject lateinit var state: TopfaceAppState
 
     init {
         setUrlAvatar(App.get().profile)
         App.get().inject(this)
-        profileSubscription = state.getObservable(Profile::class.java).subscribe { profile -> setUrlAvatar(profile) }
+        profileSubscription = state.getObservable(Profile::class.java).subscribe { profile ->
+            setUrlAvatar(profile)
+            var a = Utils.randomImageRes(profile.sex, photoCount)
+            with(a) {
+                while (this[0] == this[1]) {
+                    a = Utils.randomImageRes(profile.sex, photoCount)
+                }
+                randomLeftPhoto.set(this[0])
+                randomRightPhoto.set(this[1])
+            }
+        }
     }
 
     fun setFakeAvatar(profile: Profile) = when {
@@ -44,4 +61,5 @@ class Experiment41ViewModel(binding: LayoutExperiment41Binding) :
         super.release()
         profileSubscription.unsubscribe()
     }
+
 }
--- src/main/java/com/topface/topface/utils/Utils.java
+++ src/main/java/com/topface/topface/utils/Utils.java
@@ -18,6 +18,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.DrawableRes;
+import android.support.annotation.IntegerRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.telephony.TelephonyManager;
@@ -67,8 +68,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
+import java.util.Random;
 import java.util.regex.Pattern;
 
 public class Utils {
@@ -658,6 +661,20 @@ public class Utils {
                 }
             }
         }).exec();
+    }
+
+    public static List<Integer> randomImageRes(int sex, int count) {
+        List<Integer> result = new ArrayList<>();
+        List<Integer> girl = Arrays.asList(R.drawable.girl_1, R.drawable.girl_2, R.drawable.girl_3, R.drawable.girl_4,
+                R.drawable.girl_5, R.drawable.girl_6, R.drawable.girl_7, R.drawable.girl_8, R.drawable.girl_9, R.drawable.girl_10);
+        List<Integer> boys = Arrays.asList(R.drawable.man_1, R.drawable.man_2, R.drawable.man_3, R.drawable.man_4,
+                R.drawable.man_5, R.drawable.man_6, R.drawable.man_7, R.drawable.man_8, R.drawable.man_9, R.drawable.man_10);
+        while (count > 0) {
+            result.add(sex == Profile.BOY ? girl.get(new Random().nextInt(girl.size())) : boys.get(new Random().nextInt(boys.size())));
+            count--;
+        }
+
+        return result;
     }
 
     @NotNull
