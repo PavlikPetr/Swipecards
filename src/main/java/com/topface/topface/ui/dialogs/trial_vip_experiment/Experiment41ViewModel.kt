e1b345db05b07443cbcbfb284bd74ad7bd1a81d2
siberia87 <s.salnikov@topface.com>
2016-11-21 18:57:12 +0300

    подтянул  массив  баб и мужиков из values

--- src/main/java/com/topface/topface/ui/dialogs/trial_vip_experiment/Experiment41ViewModel.kt
+++ src/main/java/com/topface/topface/ui/dialogs/trial_vip_experiment/Experiment41ViewModel.kt
@@ -24,9 +24,9 @@ class Experiment41ViewModel(binding: LayoutExperiment41Binding) :
         const val PHOTO_COUNT = 2
     }
 
-    val girls = listOf(R.drawable.girl_1, R.drawable.girl_2, R.drawable.girl_3, R.drawable.girl_4, R.drawable.girl_5, R.drawable.girl_6, R.drawable.girl_7, R.drawable.girl_8, R.drawable.girl_9, R.drawable.girl_10)
+    val girls = R.array.fake_girl_avatars
 
-    val boys = listOf(R.drawable.man_1, R.drawable.man_2, R.drawable.man_3, R.drawable.man_4, R.drawable.man_5, R.drawable.man_6, R.drawable.man_7, R.drawable.man_8, R.drawable.man_9, R.drawable.man_10)
+    val boys = R.array.fake_boy_avatars
 
     val userAvatar: ObservableField<String> = ObservableField()
     val randomLeftPhoto: ObservableField<Int> = ObservableField()
@@ -44,7 +44,7 @@ class Experiment41ViewModel(binding: LayoutExperiment41Binding) :
     }
 
     fun setRandomPhoto(profile: Profile) =
-            with(Utils.randomImageRes(PHOTO_COUNT, if (profile.sex == Profile.BOY) girls else boys)) {
+            with(Utils.randomImageRes(PHOTO_COUNT, if (profile.sex == Profile.BOY) binding.root.resources.getIntArray(R.array.fake_girl_avatars).toMutableList() else binding.root.resources.getIntArray(R.array.fake_boy_avatars).toMutableList())) {
                 randomLeftPhoto.set(this[0])
                 randomRightPhoto.set(this[1])
             }
