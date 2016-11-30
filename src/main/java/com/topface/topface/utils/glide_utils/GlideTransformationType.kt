41532a6bf86b2b679cbb73adac5d99595470fc32
siberia87 <s.salnikov@topface.com>
2016-11-30 17:54:08 +0300

    константы с большой буквы

--- src/main/java/com/topface/topface/utils/glide_utils/GlideTransformationType.kt
+++ src/main/java/com/topface/topface/utils/glide_utils/GlideTransformationType.kt
@@ -9,12 +9,12 @@ import android.support.annotation.IntDef
 object GlideTransformationType {
 
     @Retention(AnnotationRetention.SOURCE)
-    @IntDef(CropCircleType, AdmirationType, OnlineType, AdmirationOnlineType)
+    @IntDef(CROP_CIRCLE_TYPE, ADMIRATION_TYPE, ONLINE_TYPE, ADMIRATION_OLINE_TYPE)
     annotation class GlideTransformationType
 
-    const val CropCircleType = 0L
-    const val AdmirationType = 1L
-    const val OnlineType = 2L
-    const val AdmirationOnlineType = 3L
+    const val CROP_CIRCLE_TYPE = 0L
+    const val ADMIRATION_TYPE = 1L
+    const val ONLINE_TYPE = 2L
+    const val ADMIRATION_OLINE_TYPE = 3L
 
 }
