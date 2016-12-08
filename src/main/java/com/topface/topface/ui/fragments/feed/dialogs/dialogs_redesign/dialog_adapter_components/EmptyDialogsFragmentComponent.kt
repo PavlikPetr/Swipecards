ed38cfb0519c278c09b111d0a4229abc1b637256
Tiberal <nikiti.oleg@gmail.com>
2016-12-08 12:27:32 +0300

    all dialogs empty stub, fix

---
+++ src/main/java/com/topface/topface/ui/fragments/feed/dialogs/dialogs_redesign/dialog_adapter_components/EmptyDialogsFragmentComponent.kt
@@ -0,0 +1,7 @@
+package com.topface.topface.ui.fragments.feed.dialogs.dialogs_redesign.dialog_adapter_components
+
+/**
+ * Created by tiberal on 07.12.16.
+ */
+class EmptyDialogsFragmentComponent {
+}
