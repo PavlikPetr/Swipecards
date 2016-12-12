bf217ed77507ea4b61fa3a30c33d499bf325e7c8
garastard <mb@blackbeautystudio.com>
2016-12-12 12:34:31 +0300

    Все. меню запилено согласно дизайну и православным канонам. Наташа лайкнула, замечаний нет

--- src/main/java/com/topface/topface/ui/fragments/feed/dialogs/DialogMenuFragment.kt
+++ src/main/java/com/topface/topface/ui/fragments/feed/dialogs/DialogMenuFragment.kt
@@ -1,11 +1,14 @@
 package com.topface.topface.ui.fragments.feed.dialogs
 
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.view.LayoutInflater
 import android.view.View
-import com.topface.topface.App
+import android.view.ViewGroup
+import android.view.Window
 import com.topface.topface.R
 import com.topface.topface.data.FeedDialog
 import com.topface.topface.databinding.DeleteOrBlacklistPopupBinding
-import com.topface.topface.ui.dialogs.BaseDialog
 import com.topface.topface.ui.fragments.feed.dating.IDialogCloser
 import com.topface.topface.ui.fragments.feed.feed_api.DeleteFeedRequestFactory
 import com.topface.topface.ui.fragments.feed.feed_api.FeedApi
@@ -14,14 +17,22 @@ import com.topface.topface.utils.http.IRequestClient
 /**
  * Created by mbulgakov on 06.12.16.
  */
-class DialogMenuFragment(val item: FeedDialog) : BaseDialog(), IDialogCloser {
+class DialogMenuFragment(val item: FeedDialog) : DialogFragment(), IDialogCloser {
 
     companion object {
         fun newInstance(item: FeedDialog) = DialogMenuFragment(item)
     }
 
-
-    override fun getDialogStyleResId(): Int = 0
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val resLayoutId = getDialogLayoutRes()
+        var root: View? = null
+        if (resLayoutId != 0) {
+            root = inflater!!.inflate(resLayoutId, container, false)
+            initViews(root)
+        }
+        dialog.window.requestFeature(Window.FEATURE_NO_TITLE)
+        return root
+    }
 
     private lateinit var mBinding: DeleteOrBlacklistPopupBinding
 
@@ -37,12 +48,12 @@ class DialogMenuFragment(val item: FeedDialog) : BaseDialog(), IDialogCloser {
         DialogsMenuPopupViewModel(item, mApi, this)
     }
 
-    override fun initViews(root: View?) {
+    fun initViews(root: View?) {
         mBinding = DeleteOrBlacklistPopupBinding.bind(root)
         mBinding.setModel(mViewModel)
     }
 
-    override fun getDialogLayoutRes() = R.layout.delete_or_blacklist_popup
+    fun getDialogLayoutRes() = R.layout.delete_or_blacklist_popup
 
     override fun closeIt() = dialog.cancel()
 }
