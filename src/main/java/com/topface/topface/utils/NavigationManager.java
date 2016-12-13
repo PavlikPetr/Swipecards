d36f10d14822df5a68d2c662b8df2cd451c5b85c
MistaKilla <barmatolog@yandex.ru>
2016-12-13 12:05:20 +0300

    Restore lost import

--- src/main/java/com/topface/topface/utils/NavigationManager.java
+++ src/main/java/com/topface/topface/utils/NavigationManager.java
@@ -24,6 +24,7 @@ import com.topface.topface.ui.fragments.EditorFragment;
 import com.topface.topface.ui.fragments.IntegrationWebViewFragment;
 import com.topface.topface.ui.fragments.SettingsFragment;
 import com.topface.topface.ui.fragments.dating.DatingFragment;
+import com.topface.topface.ui.fragments.feed.TabbedDialogsFragment;
 import com.topface.topface.ui.fragments.feed.TabbedLikesFragment;
 import com.topface.topface.ui.fragments.feed.TabbedVisitorsFragment;
 import com.topface.topface.ui.fragments.feed.dialogs.dialogs_redesign.DialogsFragment;
