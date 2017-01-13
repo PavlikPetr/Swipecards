b9ee7beaa261f8a44047ba8024e5cfe7df3b3270
Tiberal <nikiti.oleg@gmail.com>
2017-01-13 19:12:17 +0300

    scroll fixes

--- src/main/java/com/topface/topface/ui/fragments/feed/toolbar/PrimalCollapseViewModel.kt
+++ src/main/java/com/topface/topface/ui/fragments/feed/toolbar/PrimalCollapseViewModel.kt
@@ -6,7 +6,9 @@ import android.view.View
 import com.topface.topface.R
 import com.topface.topface.databinding.AppBarBinding
 import com.topface.topface.ui.fragments.feed.toolbar.CustomCoordinatorLayout.ViewConfig
+import com.topface.topface.utils.extensions.appContext
 import com.topface.topface.viewModels.BaseViewModel
+import org.jetbrains.anko.dimen
 
 /**
  * Model for interaction with collapsing toolbar
@@ -17,7 +19,7 @@ class PrimalCollapseViewModel(binding: AppBarBinding,
         , AppBarLayout.OnOffsetChangedListener {
 
     // вводим коррективы для скрола в альбоме, в приоритете горизонтальный скрол
-    val viewConfigList = listOf<ViewConfig>(ViewConfig(R.id.dating_album, 1f, 0.4f, true))
+    val viewConfigList = listOf(ViewConfig(R.id.dating_album, 1f, 0.4f, true))
 
     val anchorVisibility = ObservableInt(View.VISIBLE)
     val collapseVisibility = ObservableInt(View.VISIBLE)
@@ -31,6 +33,9 @@ class PrimalCollapseViewModel(binding: AppBarBinding,
             mScrimStateListener.isScrimVisible(isScrimsAreShown)
             mScrimStateListener.isCollapsed(isCollapsed)
             shadowVisibility.set(if (isCollapsed) View.VISIBLE else View.GONE)
+            if (visiblePartSize == binding.appContext().dimen(R.dimen.dating_album_height)) {
+                mScrimStateListener.isExpanded()
+            }
         }
     }
 }
