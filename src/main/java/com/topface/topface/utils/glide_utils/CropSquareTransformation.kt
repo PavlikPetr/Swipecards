b05af933eed56ecb99ec3bb6c63b825f33b80950
garastard <mb@blackbeautystudio.com>
2017-01-18 16:08:31 +0300

    коррекция

--- src/main/java/com/topface/topface/utils/glide_utils/CropSquareTransformation.kt
+++ src/main/java/com/topface/topface/utils/glide_utils/CropSquareTransformation.kt
@@ -17,26 +17,12 @@ class CropSquareTransformation(mContext: Context) : BaseGlideTransformation(mCon
         val inBitmap = resource.get()
         val width = inBitmap.width
         val height = inBitmap.height
-        val outBitmap =
-                if (width >= height)
-                    Bitmap.createBitmap(
-                            inBitmap,
-                            width / 2 - height / 2,
-                            0,
-                            height,
-                            height
-                    )
-                else
-                    Bitmap.createBitmap(
-                            inBitmap,
-                            0,
-                            height / 2 - width / 2,
-                            width,
-                            width
-                    )
+        val outBitmap = bitmapCreating(inBitmap, width, height)
         inBitmap.recycle()
-
         return BitmapResource.obtain(outBitmap, mBitmapPool)
     }
+
+    private fun bitmapCreating(source:Bitmap, width:Int, height:Int) = if (width >= height) Bitmap.createBitmap(source, width / 2 - height / 2, 0, height, height)
+                                                                        else Bitmap.createBitmap(source, 0, height / 2 - width / 2, width, width)
 
 }
