b5c1a431128a24bed7a3eb9db8ce5c201d0fb62a
garastard <mb@blackbeautystudio.com>
2017-01-19 14:36:29 +0300

    status and photocounter

--- src/main/java/com/topface/topface/ui/fragments/dating/dating_redesign/DatingFragmentViewModel.kt
+++ src/main/java/com/topface/topface/ui/fragments/dating/dating_redesign/DatingFragmentViewModel.kt
@@ -2,6 +2,7 @@ package com.topface.topface.ui.fragments.dating.dating_redesign
 
 
 import android.databinding.ObservableField
+import android.view.View
 import com.topface.topface.App
 import com.topface.topface.R
 import com.topface.topface.data.search.SearchUser
@@ -17,6 +18,10 @@ class DatingFragmentViewModel {
     val age = ObservableField<String>()
     val city = ObservableField<String>()
     val iconOnlineRes = ObservableField<Int>()
+    val statusText = ObservableField<String>()
+    val statusVisibility = ObservableField<Int>()
+    val photoCounterVisibility = ObservableField<Int>()
+    val photoCounter = ObservableField<String>()
 
     var currentUser: SearchUser? = null
         set(value) {
@@ -26,6 +31,21 @@ class DatingFragmentViewModel {
                 age.set(String.format(App.getCurrentLocale(), AGE_TEMPLATE, value.age))
                 city.set(value.city?.name)
                 iconOnlineRes.set(if (value.online) R.drawable.ico_online else 0)
+                statusText.set(value.getStatus())
+                statusVisibility.set(if (value.getStatus().isEmpty()) View.GONE else View.VISIBLE)
+                updatePhotosCounter(0)
             }
         }
+
+    fun updatePhotosCounter(position: Int) {
+        val user = currentUser
+        if (user != null && user.photos != null && user.photos.isNotEmpty()) {
+            if (photoCounterVisibility.get() == View.GONE) {
+                photoCounterVisibility.set(View.VISIBLE)
+            }
+            photoCounter.set("${position + 1}/${user.photosCount}")
+        } else {
+            photoCounterVisibility.set(View.GONE)
+        }
+    }
 }
