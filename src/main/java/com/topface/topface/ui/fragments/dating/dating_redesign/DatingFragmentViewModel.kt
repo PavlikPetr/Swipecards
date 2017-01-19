b7f42ff034a5f66a3298109d1f9ed87c15c460c6
garastard <mb@blackbeautystudio.com>
2017-01-19 17:15:26 +0300

    правки

--- src/main/java/com/topface/topface/ui/fragments/dating/dating_redesign/DatingFragmentViewModel.kt
+++ src/main/java/com/topface/topface/ui/fragments/dating/dating_redesign/DatingFragmentViewModel.kt
@@ -6,7 +6,6 @@ import android.view.View
 import com.topface.topface.App
 import com.topface.topface.R
 import com.topface.topface.data.search.SearchUser
-import com.topface.topface.utils.Utils
 import com.topface.topface.viewModels.LeftMenuHeaderViewModel.AGE_TEMPLATE
 
 /**
@@ -15,21 +14,19 @@ import com.topface.topface.viewModels.LeftMenuHeaderViewModel.AGE_TEMPLATE
 class DatingFragmentViewModel {
 
     val name = ObservableField<String>()
-    val age = ObservableField<String>()
-    val city = ObservableField<String>()
+    val feedAge = ObservableField<String>()
+    val feedCity = ObservableField<String>()
     val iconOnlineRes = ObservableField<Int>()
     val statusText = ObservableField<String>()
     val statusVisibility = ObservableField<Int>()
-    val photoCounterVisibility = ObservableField<Int>()
     val photoCounter = ObservableField<String>()
 
     var currentUser: SearchUser? = null
         set(value) {
-            field = value
-            value?.let {
-                name.set(value.firstName ?: Utils.EMPTY)
-                age.set(String.format(App.getCurrentLocale(), AGE_TEMPLATE, value.age))
-                city.set(value.city?.name)
+            field = value?.apply {
+                name.set(value.firstName)
+                feedAge.set(String.format(App.getCurrentLocale(), AGE_TEMPLATE, value.age))
+                feedCity.set(value.city.name)
                 iconOnlineRes.set(if (value.online) R.drawable.ico_online else 0)
                 statusText.set(value.getStatus())
                 statusVisibility.set(if (value.getStatus().isEmpty()) View.GONE else View.VISIBLE)
@@ -37,15 +34,5 @@ class DatingFragmentViewModel {
             }
         }
 
-    fun updatePhotosCounter(position: Int) {
-        val user = currentUser
-        if (user != null && user.photos != null && user.photos.isNotEmpty()) {
-            if (photoCounterVisibility.get() == View.GONE) {
-                photoCounterVisibility.set(View.VISIBLE)
-            }
-            photoCounter.set("${position + 1}/${user.photosCount}")
-        } else {
-            photoCounterVisibility.set(View.GONE)
-        }
-    }
+    fun updatePhotosCounter(position: Int) = photoCounter.set("${position + 1}/${currentUser?.photosCount}")
 }
