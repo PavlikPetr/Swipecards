404dd830bec3fab8c1540d0c03516813211f4638
MistaKilla <barmatolog@yandex.ru>
2017-01-25 12:29:27 +0300

    Add option for translucent statusBar and support for it

---
+++ src/main/java/com/topface/topface/data/experiments/NewDatingDesign.kt
@@ -0,0 +1,35 @@
+package com.topface.topface.data.experiments
+
+import android.os.Build
+import android.os.Parcel
+import android.os.Parcelable
+
+/**
+ * Experiment config for new dating
+ * Created by m.bayutin on 24.01.17.
+ */
+class NewDatingDesign : BaseExperiment {
+
+    constructor() : super()
+    constructor(_in: Parcel) : super(_in)
+
+    val isKitKatWithNoTranslucent: Boolean
+        get() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT
+                && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP
+                && !isEnabled
+
+    companion object {
+        @JvmField val CREATOR: Parcelable.Creator<NewDatingDesign> = object : Parcelable.Creator<NewDatingDesign> {
+            override fun createFromParcel(source: Parcel): NewDatingDesign = NewDatingDesign(source)
+            override fun newArray(size: Int): Array<NewDatingDesign?> = arrayOfNulls(size)
+        }
+    }
+
+    override fun isEnabled() =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) super.isEnabled() else false
+
+    override fun getOptionsKey(): String {
+        // todo fill with correct value
+        return "newDatingDesign"
+    }
+}
