1de05618dd4a17006e6cf7861151a681b4682cf3
garastard <mb@blackbeautystudio.com>
2017-01-30 19:06:35 +0300

    лишнее от теста осталось

--- src/main/java/com/topface/topface/utils/NavigationManager.java
+++ src/main/java/com/topface/topface/utils/NavigationManager.java
@@ -195,10 +195,9 @@ public class NavigationManager {
                         new DatingFragment();
                 break;
             case FragmentIdData.GEO:
-                fragment =
-// App.get().getOptions().peopleNearbyRedesignEnabled ?
-                        new com.topface.topface.ui.fragments.feed.people_nearby.people_nerby_redesign.PeopleNearbyFragment() ;
-//                        new PeopleNearbyFragment();
+                fragment = App.get().getOptions().peopleNearbyRedesignEnabled ?
+                        new com.topface.topface.ui.fragments.feed.people_nearby.people_nerby_redesign.PeopleNearbyFragment() :
+                        new PeopleNearbyFragment();
                 break;
             case FragmentIdData.BONUS:
                 fragment = BonusFragment.newInstance(true);
