74d27cfcdd26c2f467ab06740c3b3cfdfd36de8a
MistaKilla <barmatolog@yandex.ru>
2017-02-03 16:33:08 +0300

    Add instant purchase support for app-of-day items

--- src/main/java/com/topface/topface/ui/fragments/feed/app_day/AppDayItemViewModel.kt
+++ src/main/java/com/topface/topface/ui/fragments/feed/app_day/AppDayItemViewModel.kt
@@ -2,6 +2,7 @@ package com.topface.topface.ui.fragments.feed.app_day
 
 import android.content.Context
 import android.databinding.ObservableField
+import com.topface.billing.InstantPurchaseModel
 import com.topface.framework.utils.Debug
 import com.topface.topface.statistics.AppBannerStatistics
 import com.topface.topface.utils.Utils
@@ -10,13 +11,43 @@ import com.topface.topface.utils.Utils
  * VM итема рекламы
  * Created by siberia87 on 06.10.16.
  */
-class AppDayItemViewModel(private val mContext: Context, val image: AppDayImage) {
+class AppDayItemViewModel(private val mContext: Context, val image: AppDayImage, private val mInstantPurchaseModel: InstantPurchaseModel) {
+    companion object {
+        private const val WEBVIEW = 1;
+        private const val BROWSER = 2;
+        private const val PRODUCT = 3;
+    }
+
     val TAG_LOG = "app_of_the_day_banner_clicked"
 
     val iconUrl = ObservableField(image.imgSrc)
+
     fun onBannerClick() {
-        Utils.goToUrl(mContext, image.url)
+        when(image.showType) {
+            PRODUCT-> {
+                if (image.sku.isEmpty()) openLink(image) else {
+                    with(mInstantPurchaseModel) {
+                        this.navigator.showPurchaseProduct(
+                                skuId = image.sku,
+                                from = this.from,
+                                isSubscription = this.isSubscription
+                        )
+                    }
+                }
+            }
+            // ранее тут должно было использоваться поле image.external {@link AppDayImage.external}
+            // но оно не использовалось
+            // видимо задел на будущее
+            // теперь для разделения надо будет использовать WEBVIEW и BROWSER
+            WEBVIEW, BROWSER -> {
+                openLink(image)
+            }
+            // в непредвиденной ситуации попробуем тоже открыть линк
+            else -> openLink(image)
+        }
         AppBannerStatistics.sendBannerClicked(image.id)
         Debug.log(TAG_LOG, image.id)
     }
+
+    private fun openLink(image: AppDayImage) = Utils.goToUrl(mContext, image.url)
 }
--- src/main/java/com/topface/topface/ui/fragments/feed/dialogs/DialogsAdapter.kt
+++ src/main/java/com/topface/topface/ui/fragments/feed/dialogs/DialogsAdapter.kt
@@ -1,6 +1,7 @@
 package com.topface.topface.ui.fragments.feed.dialogs
 
 import android.databinding.ViewDataBinding
+import com.topface.billing.InstantPurchaseModel
 import com.topface.topface.BR
 import com.topface.topface.R
 import com.topface.topface.data.FeedDialog
@@ -9,9 +10,8 @@ import com.topface.topface.databinding.FeedItemDialogBinding
 import com.topface.topface.ui.fragments.feed.app_day.AppDayImage
 import com.topface.topface.ui.fragments.feed.app_day.AppDayViewModel
 import com.topface.topface.ui.fragments.feed.feed_base.BaseFeedAdapter
-import com.topface.topface.ui.fragments.feed.feed_base.IFeedNavigator
 
-class DialogsAdapter(private val mNavigator: IFeedNavigator) :
+class DialogsAdapter(private val mPurchaseModel: InstantPurchaseModel) :
         BaseFeedAdapter<FeedItemDialogBinding, FeedDialog>() {
 
     override fun getItemLayout() = R.layout.feed_item_dialog
@@ -20,7 +20,7 @@ class DialogsAdapter(private val mNavigator: IFeedNavigator) :
         super.bindData(binding, position)
         binding?.let { bind ->
             getDataItem(position)?.let {
-                binding.model = DialogsItemViewModel(bind, it, mNavigator) { isActionModeEnabled }
+                binding.model = DialogsItemViewModel(bind, it, mPurchaseModel.navigator) { isActionModeEnabled }
             }
         }
     }
@@ -28,7 +28,7 @@ class DialogsAdapter(private val mNavigator: IFeedNavigator) :
     override fun bindHeader(binding: ViewDataBinding?, position: Int) {
         binding?.let { bind ->
             (getHeaderItem(position) as? List<AppDayImage>)?.let {
-                bind.setVariable(BR.viewModel, AppDayViewModel(bind as AppDayListBinding, it))
+                bind.setVariable(BR.viewModel, AppDayViewModel(bind as AppDayListBinding, it, mPurchaseModel))
             }
         }
     }
