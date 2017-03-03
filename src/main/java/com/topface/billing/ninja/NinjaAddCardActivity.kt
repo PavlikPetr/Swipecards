45477b0a49bb71093d756d64a30b46500605c647
MistaKilla <barmatolog@yandex.ru>
2017-03-03 15:20:23 +0300

    Refactor by comments

--- src/main/java/com/topface/billing/ninja/NinjaAddCardActivity.kt
+++ src/main/java/com/topface/billing/ninja/NinjaAddCardActivity.kt
@@ -4,18 +4,16 @@ import com.topface.topface.R
 import com.topface.topface.databinding.LayoutNinjaAddCardBinding
 import com.topface.topface.databinding.ToolbarBinding
 import com.topface.topface.ui.BaseFragmentActivity
-import android.view.View
+import com.topface.topface.ui.views.toolbar.view_models.EmptyToolbarViewModel
 
 /**
  * Add bank card activity
  * Created by m.bayutin on 02.03.17.
  */
 class NinjaAddCardActivity: BaseFragmentActivity<LayoutNinjaAddCardBinding>() {
-    override fun getToolbarBinding(binding: LayoutNinjaAddCardBinding): ToolbarBinding {
-        // дада, отдаем биндинг и скрываем вьюху, ибо этот метод _надо_ реализовать, so sad
-        binding.toolbarInclude.root?.visibility = View.GONE
-        return binding.toolbarInclude
-    }
+    override fun getToolbarBinding(binding: LayoutNinjaAddCardBinding) = binding.toolbarInclude
+
+    override fun generateToolbarViewModel(toolbar: ToolbarBinding) = EmptyToolbarViewModel(toolbar)
 
     override fun getLayout(): Int = R.layout.layout_ninja_add_card
 }
