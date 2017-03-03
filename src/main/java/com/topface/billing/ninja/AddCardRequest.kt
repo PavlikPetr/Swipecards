7823b910602df2e7922ec77c14f93967d68be762
MistaKilla <barmatolog@yandex.ru>
2017-03-03 14:57:25 +0300

    Refactor by comments

--- src/main/java/com/topface/billing/ninja/AddCardRequest.kt
+++ src/main/java/com/topface/billing/ninja/AddCardRequest.kt
@@ -1,13 +1,10 @@
 package com.topface.billing.ninja
 
 import android.content.Context
-import com.topface.topface.requests.ApiRequest
 import com.topface.topface.requests.IApiResponse
 import com.topface.topface.requests.handlers.SimpleApiHandler
 import com.topface.topface.ui.external_libs.offers.OffersUtils
-import org.json.JSONObject
-import retrofit2.http.GET
-import retrofit2.http.QueryMap
+import rx.Emitter
 import rx.Observable
 import java.util.*
 
@@ -25,51 +22,28 @@ class AddCardRequest {
         const val KEY_SECURITY_CODE = "security_code"
     }
 
-    fun getRequestObservable(context: Context, addCardModel: AddCardModel): Observable<IApiResponse> {
-        val params = HashMap<String, String>()
-        params.put(KEY_PROJECT, addCardModel.project)
-        params.put(KEY_NUMBER, addCardModel.number)
-        params.put(KEY_EXP_MONTH, addCardModel.expirationMonth)
-        params.put(KEY_EXP_YEAR, addCardModel.expirationYear)
-        params.put(KEY_SECURITY_CODE, addCardModel.securityCode)
-        return OffersUtils
-                .getRequestInstance(ADD_CARD_LINK)
-                .create(Request::class.java)
-                .setParams(params)
-                .flatMap { addCardResponse -> getSendCardTokenRequestObservable(context, addCardModel.email, addCardResponse) }
-    }
+    fun getRequestObservable(context: Context, addCardModel: AddCardModel) =
+        OffersUtils.getRequestInstance(ADD_CARD_LINK)
+            .create(Request::class.java)
+            .setParams(HashMap<String, String>().apply{
+                put(KEY_PROJECT, addCardModel.project)
+                put(KEY_NUMBER, addCardModel.number)
+                put(KEY_EXP_MONTH, addCardModel.expirationMonth)
+                put(KEY_EXP_YEAR, addCardModel.expirationYear)
+                put(KEY_SECURITY_CODE, addCardModel.securityCode)
+            })
+            .flatMap { addCardResponse -> getSendCardTokenRequestObservable(context, addCardModel.email, addCardResponse) }
 
-    private fun getSendCardTokenRequestObservable(context: Context, email: String, addCardResponse: AddCardResponse): Observable<IApiResponse> {
-        return Observable.create {
+    private fun getSendCardTokenRequestObservable(context: Context, email: String, addCardResponse: AddCardResponse) =
+        Observable.fromEmitter<IApiResponse>( { emitter ->
             val sendRequest = SendCardTokenRequest(context, SendCardTokenModel(addCardResponse.id, email))
-            sendRequest.callback(object : SimpleApiHandler() {
-                override fun success(response: IApiResponse) = it.onNext(response)
-                override fun fail(codeError: Int, response: IApiResponse) = it.onError(Throwable(codeError.toString()))
-                override fun always(response: IApiResponse) {
-                    super.always(response)
-                    it.onCompleted()
-                }
-            }).exec()
-        }
-    }
-
-    private interface Request {
-        @GET(ADD_CARD_LINK)
-        fun setParams(@QueryMap params: Map<String, String>): Observable<AddCardResponse>
-    }
-
-    private class SendCardTokenRequest(context: Context, val data: SendCardTokenModel) : ApiRequest(context) {
-        override fun getServiceName() = SERVICE_NAME
-
-        override fun getRequestData() = JSONObject().apply {
-            put(KEY_EMAIL, data.email)
-            put(KEY_TOKEN, data.token)
-        }
-
-        companion object {
-            const val SERVICE_NAME = "paymentNinja.addCard"
-            const val KEY_TOKEN = "token"
-            const val KEY_EMAIL = "email"
-        }
-    }
+                sendRequest.callback(object : SimpleApiHandler() {
+                    override fun success(response: IApiResponse) = emitter.onNext(response)
+                    override fun fail(codeError: Int, response: IApiResponse) = emitter.onError(Throwable(codeError.toString()))
+                    override fun always(response: IApiResponse) {
+                        super.always(response)
+                        emitter.onCompleted()
+                    }
+                }).exec()
+            }, Emitter.BackpressureMode.NONE)
 }
