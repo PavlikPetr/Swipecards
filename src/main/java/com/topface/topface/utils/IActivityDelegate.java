d0ca6580a25c68a8c3b82db65313015cec9fbe1b
garastard <mb@blackbeautystudio.com>
2017-04-12 09:44:53 +0300

    correction

--- src/main/java/com/topface/topface/utils/IActivityDelegate.java
+++ src/main/java/com/topface/topface/utils/IActivityDelegate.java
@@ -1,13 +1,10 @@
 package com.topface.topface.utils;
 
-
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v4.app.FragmentManager;
-
-
 
 /**
  * Делегат для актиити. Можно дописывать лдругие методы активити
