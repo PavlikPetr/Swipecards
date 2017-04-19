ee8d18af10fd7742623c0e2744c749f2feb02e57
garastard <mb@blackbeautystudio.com>
2017-04-19 16:46:45 +0300

    коррекция

--- src/main/java/com/topface/topface/ui/fragments/feed/enhanced/chat/LoaderStubComponent.kt
+++ src/main/java/com/topface/topface/ui/fragments/feed/enhanced/chat/LoaderStubComponent.kt
@@ -2,19 +2,19 @@ package com.topface.topface.ui.fragments.feed.enhanced.chat
 
 import android.support.v7.widget.StaggeredGridLayoutManager
 import com.topface.topface.R
-import com.topface.topface.databinding.LoaderStubChatBinding
+import com.topface.topface.databinding.ItemLoaderBinding
 import com.topface.topface.ui.new_adapter.enhanced.AdapterComponent
 
 /**
  * Компонент для итема с лоадером
  */
-class LoaderStubComponent : AdapterComponent<LoaderStubChatBinding, ChatLoader>() {
+class LoaderStubComponent : AdapterComponent<ItemLoaderBinding, ChatLoader>() {
     override val itemLayout: Int
-        get() = R.layout.loader_stub_chat
-    override val bindingClass: Class<LoaderStubChatBinding>
-        get() = LoaderStubChatBinding::class.java
+        get() = R.layout.item_loader
+    override val bindingClass: Class<ItemLoaderBinding>
+        get() = ItemLoaderBinding::class.java
 
-    override fun bind(binding: LoaderStubChatBinding, data: ChatLoader?, position: Int) {
+    override fun bind(binding: ItemLoaderBinding, data: ChatLoader?, position: Int) {
         binding.root.layoutParams = StaggeredGridLayoutManager.LayoutParams(StaggeredGridLayoutManager.LayoutParams.MATCH_PARENT,
                 StaggeredGridLayoutManager.LayoutParams.MATCH_PARENT).apply { isFullSpan = true }
     }
