e0b3c31f6e470214ed6c9e33fb798207bef569a0
garastard <mb@blackbeautystudio.com>
2017-04-19 16:40:48 +0300

    коррекция) не то растянул)
    (cherry picked from commit 01ea98e)

--- src/main/java/com/topface/topface/ui/fragments/feed/people_nearby/people_nerby_redesign/people_nearby_adapter_components/PeopleNearbyLoaderComponent.kt
+++ src/main/java/com/topface/topface/ui/fragments/feed/people_nearby/people_nerby_redesign/people_nearby_adapter_components/PeopleNearbyLoaderComponent.kt
@@ -1,6 +1,5 @@
 package com.topface.topface.ui.fragments.feed.people_nearby.people_nerby_redesign.people_nearby_adapter_components
 
-import android.support.v7.widget.StaggeredGridLayoutManager
 import com.topface.topface.R
 import com.topface.topface.databinding.ItemLoaderBinding
 import com.topface.topface.ui.fragments.feed.people_nearby.people_nerby_redesign.PeopleNearbyLoader
@@ -17,8 +16,5 @@ class PeopleNearbyLoaderComponent : AdapterComponent<ItemLoaderBinding, PeopleNe
     override val bindingClass: Class<ItemLoaderBinding>
         get() = ItemLoaderBinding::class.java
 
-    override fun bind(binding: ItemLoaderBinding, data: PeopleNearbyLoader?, position: Int) {
-        binding.root.layoutParams = StaggeredGridLayoutManager.LayoutParams(StaggeredGridLayoutManager.LayoutParams.MATCH_PARENT,
-                StaggeredGridLayoutManager.LayoutParams.MATCH_PARENT).apply { isFullSpan = true }
-    }
+    override fun bind(binding: ItemLoaderBinding, data: PeopleNearbyLoader?, position: Int) {}
 }
