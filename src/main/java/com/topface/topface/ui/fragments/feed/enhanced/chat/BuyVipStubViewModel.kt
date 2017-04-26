761f88dc43e55fdbda8aa5cc2d98581ef4a00ce9
garastard <mb@blackbeautystudio.com>
2017-04-26 14:22:53 +0300

    коррективы внесены

--- src/main/java/com/topface/topface/ui/fragments/feed/enhanced/chat/BuyVipStubViewModel.kt
+++ src/main/java/com/topface/topface/ui/fragments/feed/enhanced/chat/BuyVipStubViewModel.kt
@@ -1,6 +1,7 @@
 package com.topface.topface.ui.fragments.feed.enhanced.chat
 
 import android.databinding.ObservableField
+import android.databinding.ObservableInt
 import com.topface.topface.R
 import com.topface.topface.data.FeedUser
 import com.topface.topface.data.User
@@ -11,17 +12,19 @@ import com.topface.topface.utils.extensions.getString
 
 class BuyVipStubViewModel(private val mFeedUser: FeedUser, private val mFeedNavigator: FeedNavigator) {
 
-    val isMan = mFeedUser.sex == User.BOY
-    val title = ObservableField<String>(getTitle(mFeedUser))
-    val stubText = ObservableField<String>(if (isMan) R.string.write_to_her_only_vip.getString() else R.string.write_to_him_only_vip.getString())
+    private val mIsMan = mFeedUser.sex == User.BOY
+    val title = ObservableField(getTitle())
+    val stubText = ObservableField(getStubText())
 
     val mutualUserPhoto = ObservableField(mFeedUser.photo)
-    val mutualPlaceholderRes = ObservableField(if (isMan) R.drawable.dialogues_av_man_big else R.drawable.dialogues_av_girl_big)
+    val mutualPlaceholderRes = ObservableInt(if (mIsMan) R.drawable.dialogues_av_man_big else R.drawable.dialogues_av_girl_big)
     val outsideCircle = R.dimen.mutual_popup_stroke_outside.getDimen()
     val type = GlideTransformationType.CROP_CIRCLE_TYPE
 
-    private fun getTitle(mFeedUser: FeedUser) = String.format(
-            if (isMan) R.string.chat_buy_vip_popular_male.getString() else R.string.chat_buy_vip_popular_female.getString(), mFeedUser.firstName);
+    private fun getTitle() = String.format(
+            if (mIsMan) R.string.chat_buy_vip_popular_male.getString() else R.string.chat_buy_vip_popular_female.getString(), mFeedUser.firstName)
+
+    private fun getStubText() = if (mIsMan) R.string.write_to_her_only_vip.getString() else R.string.write_to_him_only_vip.getString()
 
     fun buyVip() = mFeedNavigator.showPurchaseVip("chat_stub")
 
--- src/main/java/com/topface/topface/ui/fragments/feed/enhanced/chat/MutualStubChatViewModel.kt
+++ src/main/java/com/topface/topface/ui/fragments/feed/enhanced/chat/MutualStubChatViewModel.kt
@@ -1,6 +1,7 @@
 package com.topface.topface.ui.fragments.feed.enhanced.chat
 
 import android.databinding.ObservableField
+import android.databinding.ObservableInt
 import com.topface.topface.App
 import com.topface.topface.R
 import com.topface.topface.data.FeedUser
@@ -12,17 +13,17 @@ import com.topface.topface.utils.extensions.getString
 
 class MutualStubChatViewModel(private val mMutualItem: FeedUser) {
 
-    val userPhoto = App.get().profile.photo
+    val userPhoto = ObservableField(App.get().profile.photo)
     val type = GlideTransformationType.CROP_CIRCLE_TYPE
-    val userPlaceholderRes = ObservableField((if (App.get().profile.sex == User.BOY) R.drawable.dialogues_av_man_big
+    val userPlaceholderRes = ObservableInt((if (App.get().profile.sex == User.BOY) R.drawable.dialogues_av_man_big
     else R.drawable.dialogues_av_girl_small))
 
     val mutualUserPhoto = ObservableField(mMutualItem.photo)
     val mutualType = GlideTransformationType.CIRCLE_AVATAR_WITH_STROKE_AROUND
-    val mutualPlaceholderRes = ObservableField(if (mMutualItem.sex == User.BOY) R.drawable.dialogues_av_man_big else R.drawable.dialogues_av_girl_big)
+    val mutualPlaceholderRes = ObservableInt(if (mMutualItem.sex == User.BOY) R.drawable.dialogues_av_man_big else R.drawable.dialogues_av_girl_big)
     val outsideCircle = R.dimen.mutual_popup_stroke_outside.getDimen()
     val circleColor = R.color.ask_moar_item_background
 
-    val stubText = ObservableField<String>((if (App.get().profile.sex == User.BOY) R.string.write_her_something_first.getString()
+    val stubText = ObservableField((if (App.get().profile.sex == User.BOY) R.string.write_her_something_first.getString()
     else R.string.write_him_first.getString()))
 }
