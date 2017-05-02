f1a71a403658921b56d3032cfea3f0160b64f6d6
Tiberal <nikiti.oleg@gmail.com>
2017-05-02 20:10:38 +0300

    add keyboard state

--- src/main/java/com/topface/topface/ui/fragments/feed/enhanced/chat/ChatFragment.kt
+++ src/main/java/com/topface/topface/ui/fragments/feed/enhanced/chat/ChatFragment.kt
@@ -1,5 +1,6 @@
 package com.topface.topface.ui.fragments.feed.enhanced.chat
 
+import android.content.res.Configuration
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.view.MenuItemCompat
@@ -16,18 +17,26 @@ import com.topface.topface.di.ComponentManager
 import com.topface.topface.di.chat.ChatComponent
 import com.topface.topface.di.chat.ChatViewModelComponent
 import com.topface.topface.di.chat.DaggerChatViewModelComponent
+import com.topface.topface.ui.fragments.ToolbarActivity
 import com.topface.topface.ui.fragments.feed.enhanced.base.IViewModelLifeCycle
 import com.topface.topface.ui.fragments.feed.enhanced.base.setViewModel
 import com.topface.topface.ui.fragments.feed.enhanced.utils.DaggerFragment
 import com.topface.topface.ui.fragments.feed.feed_base.FeedNavigator
 import com.topface.topface.ui.new_adapter.enhanced.CompositeAdapter
 import com.topface.topface.ui.views.ImageViewRemote
+import com.topface.topface.ui.views.KeyboardListenerLayout
+import com.topface.topface.utils.Device
+import com.topface.topface.utils.Utils
 import com.topface.topface.utils.actionbar.OverflowMenu
 import com.topface.topface.utils.actionbar.OverflowMenuUser
 import org.jetbrains.anko.layoutInflater
 import javax.inject.Inject
 
-class ChatFragment : DaggerFragment() {
+class ChatFragment : DaggerFragment(), KeyboardListenerLayout.KeyboardListener {
+
+    companion object {
+        private const val SOFT_KEYBOARD_LOCK_STATE = "keyboard_state"
+    }
 
     @Inject lateinit var adapter: CompositeAdapter
     @Inject lateinit var navigator: FeedNavigator
@@ -46,13 +55,20 @@ class ChatFragment : DaggerFragment() {
     private var mOverflowMenu: OverflowMenu? = null
     private var mBarAvatar: MenuItem? = null
     private var mUser: FeedUser? = null
+    private var mKeyboardWasShown = false // по умолчанию клава в чате закрыта
 
     override fun getViewModel(): IViewModelLifeCycle = mViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mKeyboardWasShown = savedInstanceState?.getBoolean(SOFT_KEYBOARD_LOCK_STATE) ?: false
         mUser = arguments?.getParcelable(ChatIntentCreator.WHOLE_USER)
         ComponentManager.obtainComponent(ChatComponent::class.java).inject(this)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putBoolean(SOFT_KEYBOARD_LOCK_STATE, mKeyboardWasShown)
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -61,11 +77,18 @@ class ChatFragment : DaggerFragment() {
             chat.layoutManager = LinearLayoutManager(context.applicationContext)
             chat.adapter = adapter
             setViewModel(BR.chatViewModel, mViewModel, arguments)
+            root.setKeyboardListener(this@ChatFragment)
         }.root
     }
 
     override fun terminateImmortalComponent() {
         ComponentManager.releaseComponent(ChatViewModelComponent::class.java)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //показать клавиатуру, если она была показаны до этого(перешли в другой фрагмент, и вернулись обратно)
+        showKeyboard()
     }
 
     override fun onDestroyView() {
@@ -153,4 +176,58 @@ class ChatFragment : DaggerFragment() {
         overflowMenu.initOverfowMenu()
     }
 
+    //Фокусы с клавиатурой! Это та еще адуха, пусть будет тут. Дабы не захламлять VM
+    override fun keyboardOpened() {
+        mKeyboardWasShown = true
+        if (isAdded && getScreenOrientation() == Configuration.ORIENTATION_LANDSCAPE) {
+            setActionbarVisibility(false)
+        }
+    }
+
+    override fun keyboardClosed() {
+        mKeyboardWasShown = false
+        if (isAdded) {
+            setActionbarVisibility(true)
+        }
+    }
+
+    override fun keyboardChangeState() {
+        if (isAdded && getScreenOrientation() == Configuration.ORIENTATION_PORTRAIT) {
+            setActionbarVisibility(true)
+        }
+    }
+
+    private fun showKeyboard() {
+        if (mBinding.input != null && mKeyboardWasShown) {
+            mBinding.input.postDelayed({
+                //грязный хак, чтоб отработал InputMethodManager. иначе не клава не отрабатывает и не открвается.
+                if (isShowKeyboardInChat()) {
+                    Utils.showSoftKeyboard(activity.applicationContext, mBinding.input)
+                }
+            }, 200)
+        }
+    }
+
+    private fun isShowKeyboardInChat() = if (isAdded) {
+        val context = activity.applicationContext
+        val displayMetrics = Device.getDisplayMetrics(context)
+        val height = if (getScreenOrientation() == Configuration.ORIENTATION_PORTRAIT)
+            displayMetrics.heightPixels
+        else
+            displayMetrics.widthPixels
+        val dpHeight = height / displayMetrics.density
+        dpHeight >= context.resources.getInteger(R.integer.min_screen_height_chat_fragment)
+    } else {
+        false
+    }
+
+    private fun getScreenOrientation() = App.get().resources.configuration.orientation
+
+    private fun setActionbarVisibility(visible: Boolean) {
+        val toolbarActivity = activity
+        if (toolbarActivity != null && toolbarActivity is ToolbarActivity<*>
+                && (toolbarActivity.isToolBarVisible() != visible)) {
+            toolbarActivity.setToolBarVisibility(visible)
+        }
+    }
 }
