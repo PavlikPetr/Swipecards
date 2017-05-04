3a8e6b76cbd7642ddb787f696b3c45d262171004
MistaKilla <barmatolog@yandex.ru>
2017-05-04 17:09:30 +0300

    Add clicakble avatars to friend chat items

--- src/main/java/com/topface/topface/ui/fragments/feed/enhanced/chat/items/FriendMessageComponent.kt
+++ src/main/java/com/topface/topface/ui/fragments/feed/enhanced/chat/items/FriendMessageComponent.kt
@@ -1,11 +1,12 @@
 package com.topface.topface.ui.fragments.feed.enhanced.chat.items
 
 import com.topface.topface.R
+import com.topface.topface.data.FeedUser
 import com.topface.topface.databinding.ItemChatD1FriendMessageBinding
 import com.topface.topface.ui.fragments.feed.enhanced.chat.FriendMessage
 import com.topface.topface.ui.new_adapter.enhanced.AdapterComponent
 
-class FriendMessageComponent : AdapterComponent<ItemChatD1FriendMessageBinding, FriendMessage>() {
+class FriendMessageComponent(val feedUser: FeedUser?) : AdapterComponent<ItemChatD1FriendMessageBinding, FriendMessage>() {
     override val itemLayout: Int
         get() = R.layout.item_chat_d1_friend_message
     override val bindingClass: Class<ItemChatD1FriendMessageBinding>
@@ -13,7 +14,7 @@ class FriendMessageComponent : AdapterComponent<ItemChatD1FriendMessageBinding, 
 
     override fun bind(binding: ItemChatD1FriendMessageBinding, data: FriendMessage?, position: Int) {
         data?.let {
-            binding.viewModel = FriendMessageViewModel(it, position)
+            binding.viewModel = FriendMessageViewModel(it, position, feedUser)
         }
     }
 }
