87ba132645780c38235d505114c35196c0848d8b
Tiberal <nikiti.oleg@gmail.com>
2017-05-29 14:27:17 +0300

    splash

--- src/main/java/com/topface/topface/ui/splash_screen/SplashScreenActivity.kt
+++ src/main/java/com/topface/topface/ui/splash_screen/SplashScreenActivity.kt
@@ -1,38 +1,42 @@
 package com.topface.topface.ui.splash_screen
 
-import android.animation.ObjectAnimator
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.graphics.Rect
 import android.os.Bundle
+import android.os.Handler
+import android.view.ViewPropertyAnimator
 import android.view.Window
 import com.topface.topface.R
 import com.topface.topface.databinding.AcSplashBinding
+import com.topface.topface.ui.NavigationActivity
 import com.topface.topface.ui.analytics.TrackedFragmentActivity
 import com.topface.topface.utils.extensions.getDimen
 
 
 class SplashScreenActivity : TrackedFragmentActivity() {
 
-    lateinit var binding: AcSplashBinding
+    companion object {
+        private const val PASSED_TIME = "passed_time"
+        private const val ANIMATION_DURATION = 3000L
+    }
 
-    lateinit var animator: ObjectAnimator
+    private lateinit var binding: AcSplashBinding
+    private var mAnimator: ViewPropertyAnimator? = null
+    private val mHandler = Handler()
+    private val mRunnable = { startActivity(createMainActivityIntent(NavigationActivity::class.java)) }
+    private var startTime = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView<AcSplashBinding>(this, R.layout.ac_splash)
         if (savedInstanceState == null) {
             binding.logo.post {
-
-                animator = ObjectAnimator.ofFloat(binding.logo, "translationY", binding.logo.top.toFloat(),
-                        100f + getStatusBarHeight()).apply {
-                    duration = 5000
-                    start()
-                }
-
-                /*binding.logo.alpha = 0f
-                with(binding.logo.animate()) {
-                    duration = 2000
+                binding.logo.alpha = 0f
+                mAnimator = binding.logo.animate().apply {
+                    duration = ANIMATION_DURATION
                     alpha(1f)
                     translationY(-(binding.logo.top - R.dimen.tf_logo_top.getDimen()) + getStatusBarHeight())
                     setListener(object : AnimatorListenerAdapter() {
@@ -40,23 +44,22 @@ class SplashScreenActivity : TrackedFragmentActivity() {
                             startActivity(createMainActivityIntent(NavigationActivity::class.java))
                         }
                     })
-                }*/
+                    startTime = System.currentTimeMillis()
+                }
             }
         } else {
-
-            //-(binding.logo.top - R.dimen.tf_logo_top.getDimen())
-
-            animator = ObjectAnimator.ofFloat(binding.logo, "translationY", binding.logo.top.toFloat(),
-                    100f + getStatusBarHeight()).apply {
-                duration = 5000
-                currentPlayTime = 2500
-                start()
-            }
+            mHandler.postDelayed(mRunnable, savedInstanceState.getLong(PASSED_TIME))
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putLong(PASSED_TIME, System.currentTimeMillis() - startTime)
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        mHandler.removeCallbacks(mRunnable)
     }
 
     fun getStatusBarHeight(): Int {
@@ -65,10 +68,6 @@ class SplashScreenActivity : TrackedFragmentActivity() {
         }
         val titleBarHeight = window.findViewById(Window.ID_ANDROID_CONTENT).top - rectangle.top
         return Math.abs(titleBarHeight)
-    }
-
-    override fun onResume() {
-        super.onResume()
     }
 
     private fun <T> createMainActivityIntent(clazz: Class<T>) = Intent(applicationContext, clazz)
