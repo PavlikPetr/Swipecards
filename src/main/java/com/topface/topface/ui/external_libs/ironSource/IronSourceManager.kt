29df6508714e02a392aa21865a8b067ed05e64da
PavlikPetr <petr.pavlik0205@gmail.com>
2017-06-27 17:35:57 +0300

    #56743: purchases plc

--- src/main/java/com/topface/topface/ui/external_libs/ironSource/IronSourceManager.kt
+++ src/main/java/com/topface/topface/ui/external_libs/ironSource/IronSourceManager.kt
@@ -9,6 +9,7 @@ import com.ironsource.mediationsdk.sdk.OfferwallListener
 import com.topface.framework.utils.Debug
 import com.topface.statistics.android.Slices
 import com.topface.statistics.generated.IronSourceStatisticsGeneratedStatistics
+import com.topface.topface.App
 import com.topface.topface.utils.rx.shortSubscription
 import rx.Emitter
 import rx.Observable
@@ -75,6 +76,7 @@ class IronSourceManager {
 
     fun initSdk(activity: Activity) {
         IronSource.init(activity, APP_KEY, IronSource.AD_UNIT.OFFERWALL)
+        IronSource.setUserId(App.get().profile.uid.toString())
         IntegrationHelper.validateIntegration(activity);
     }
 
