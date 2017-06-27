1d38b13f774d832fdad79a9e9b047a7c288640ee
PavlikPetr <petr.pavlik0205@gmail.com>
2017-06-27 11:32:00 +0300

    #56734: statistics

--- src/main/java/com/topface/topface/statistics/PromoDialogUniqueStatistics.java
+++ src/main/java/com/topface/topface/statistics/PromoDialogUniqueStatistics.java
@@ -2,7 +2,6 @@ package com.topface.topface.statistics;
 
 import com.topface.statistics.android.StatisticsTracker;
 import com.topface.topface.App;
-import com.topface.topface.utils.CacheProfile;
 
 /**
  * Promo popup show unique statistics
@@ -11,10 +10,10 @@ public class PromoDialogUniqueStatistics {
 
     public static final String PROMO_DIALOG_SHOW_UNIQUE = "promo_dialog_show_unique";
 
-    public static void send(String tag) {
+    public static void send(String tag, String type) {
         StatisticsTracker
                 .getInstance()
                 .setContext(App.getContext())
-                .sendUniqueEvent(PROMO_DIALOG_SHOW_UNIQUE, 1, PromoDialogStastics.getSlices(tag), Integer.toString(App.get().getProfile().uid) + "_" + tag);
+                .sendUniqueEvent(PROMO_DIALOG_SHOW_UNIQUE, 1, PromoDialogStatistics.getSlices(tag, type), Integer.toString(App.get().getProfile().uid) + "_" + tag);
     }
 }
